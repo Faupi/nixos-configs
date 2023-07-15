4910123209c253e02d409deb560c67b53fe95480
Faupi <matej.sp583@gmail.com>
2023-07-15 20:31:07 +0200

    yeeted some default gnome packages

--- cfgs/sandbox/default.nix
+++ cfgs/sandbox/default.nix
@@ -12,6 +12,22 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = [ 
+    pkgs.gnome.cheese
+    pkgs.gnome-photos
+    pkgs.gnome.gnome-music
+    # pkgs.gnome.gnome-terminal
+    # pkgs.gnome.gedit
+    pkgs.epiphany  # Web
+    # pkgs.evince
+    pkgs.gnome.gnome-characters
+    # pkgs.gnome.totem
+    pkgs.gnome.tali
+    pkgs.gnome.iagno
+    pkgs.gnome.hitori
+    pkgs.gnome.atomix
+    pkgs.gnome-tour
+  ];
 
   # Enable sound with pipewire.
   sound.enable = true;
