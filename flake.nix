9f8678fdbbe53c17e11edea6467361629326bfc7
Faupi <matej.sp583@gmail.com>
2023-07-15 23:54:44 +0200

    Fixed nixpkgs version

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     home-manager = {
       url = "github:nix-community/home-manager";
