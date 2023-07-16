b67ba249a1a83023d2110ddb2d0b9876ac99799d
Faupi <matej.sp583@gmail.com>
2023-07-16 12:27:23 +0200

    another locale

--- cfgs/base/default.nix
+++ cfgs/base/default.nix
@@ -25,17 +25,6 @@
   # Localization
   time.timeZone = "Europe/Prague";
   i18n.defaultLocale = "en_DK.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_DK.UTF-8";
-    LC_IDENTIFICATION = "en_DK.UTF-8";
-    LC_MEASUREMENT = "en_DK.UTF-8";
-    LC_MONETARY = "en_DK.UTF-8";
-    LC_NAME = "en_DK.UTF-8";
-    LC_NUMERIC = "en_DK.UTF-8";
-    LC_PAPER = "en_DK.UTF-8";
-    LC_TELEPHONE = "en_DK.UTF-8";
-    LC_TIME = "en_DK.UTF-8";
-  };
 
   # X11 keymap
   services.xserver = {
