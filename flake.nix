f05a63f7ae13185a28c4013e8f37ada9d037acbc
Faupi <matej.sp583@gmail.com>
2023-07-16 12:14:09 +0200

    test

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/nixos-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
