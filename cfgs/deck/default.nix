2c04a12aad15c11d76b257b4dfd1c4b18aa137d6
Faupi <matej.sp583@gmail.com>
2023-07-17 21:38:16 +0200

    idk??

--- cfgs/deck/default.nix
+++ cfgs/deck/default.nix
@@ -172,6 +172,7 @@ in
           protonup
           lutris
         ];
+        home.homeDirectory = config.users.users.deck.home;
         home.stateVersion = config.system.stateVersion;
       };
     };
