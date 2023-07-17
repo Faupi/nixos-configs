bd8de782ab130bbfbfe12d12a97f9cffa286acc9
Faupi <matej.sp583@gmail.com>
2023-07-17 23:04:49 +0200

    autologin

--- cfgs/deck/default.nix
+++ cfgs/deck/default.nix
@@ -104,6 +104,14 @@ in
     steamdeck-firmware
   ];
 
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "faupi";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
   # User 
   home-manager = {
     useGlobalPkgs = true;
