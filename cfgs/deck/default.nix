094f0b1e1fb357ca96de079ad4723a6fe52f044f
Faupi <matej.sp583@gmail.com>
2023-07-19 21:27:46 +0200

    mkafter tearfree

--- cfgs/deck/default.nix
+++ cfgs/deck/default.nix
@@ -293,19 +293,13 @@ in
   # Fix for KDE
   programs.ssh.askPassword = lib.mkForce "${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass";
 
-  environment.etc = {
-    "X11/xorg.conf.d/90-faupi.conf".text = ''
-      Section "Device"
-              Identifier      "AMD Graphics"
-              Driver          "amdgpu"
-              Option          "TearFree" "true"
-      EndSection
-    '';
-  };
-
-  # services.xserver.deviceSection = ''
-  #   Option "TearFree" "true"
-  # '';
+  services.xserver.config = lib.mkAfter ''
+    Section "Device"
+            Identifier      "AMD Graphics"
+            Driver          "amdgpu"
+            Option          "TearFree" "true"
+    EndSection
+  '';
 
   system.stateVersion = "23.05";
 }
