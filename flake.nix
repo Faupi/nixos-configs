f1500db44902d84c093f4cf6cf5faef0f11c1270
Faupi <matej.sp583@gmail.com>
2023-07-20 19:00:03 +0200

    jovian test

--- flake.nix
+++ flake.nix
@@ -19,10 +19,13 @@
     #   sha256 = "sha256:0b03m1f6zixw1z9bz4iac8m8d91ynsm4im5dsjgz1z4xzb6ygsbl";
     # };
 
-    jovian = builtins.fetchTarball {
-      url = "https://github.com/Jovian-Experiments/Jovian-NixOS/archive/8a934c6ebf10d0a153f0b62d933f7946e67f610f.tar.gz";
-      sha256 = "sha256:0f06vjsfppjwk4m94ma1wqakfc7fdl206db39n1hsiwp43qz7r7x";
+    jovian = {
+      url = "github:Jovian-Experiments/Jovian-NixOS/8a934c6ebf10d0a153f0b62d933f7946e67f610f";
     };
+    # jovian = builtins.fetchTarball {
+    #   url = "https://github.com/Jovian-Experiments/Jovian-NixOS/archive/8a934c6ebf10d0a153f0b62d933f7946e67f610f.tar.gz";
+    #   sha256 = "sha256:0f06vjsfppjwk4m94ma1wqakfc7fdl206db39n1hsiwp43qz7r7x";
+    # };
   };
 
   outputs = { self, nixpkgs, home-manager, plasma-manager, jovian }: {
