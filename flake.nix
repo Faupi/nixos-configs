486a636cc8519037a61dae4ef7ca28fc072abf8a
Faupi <matej.sp583@gmail.com>
2023-07-20 19:59:28 +0200

    locked HM version again

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
