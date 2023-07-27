d0c521253be617a57fe65b9f9fd0041430ff6386
Faupi <matej.sp583@gmail.com>
2023-07-27 22:21:55 +0200

    Updated video bridge

--- pkgs/xwaylandvideobridge.nix
+++ pkgs/xwaylandvideobridge.nix
@@ -5,16 +5,16 @@
 , extra-cmake-modules
 , qt5
 , libsForQt5
-}: stdenv.mkDerivation rec {
+}: stdenv.mkDerivation {
   pname = "xwaylandvideobridge";
-  version = "unstable-2023-03-28";
+  version = "unstable-2023-07-24";
 
   src = fetchFromGitLab {
     domain = "invent.kde.org";
-    owner = "davidedmundson";
+    owner = "system";
     repo = "xwaylandvideobridge";
-    rev = "b876b5f3ee5cc810c99b08e8f0ebb29553e45e47";
-    hash = "sha256-gfQkOIZegxdFQ9IV2Qp/lLRtfI5/g6bDD3XRBdLh4q0=";
+    rev = "014111b4db6298968204fd56d5ce6f691137bf90";
+    sha256 = "sha256-6ro5Y5GSiodzbsiPQls4IAOYGLcLE7j+0eumpW6+HeU=";
   };
 
   nativeBuildInputs = [
@@ -29,14 +29,14 @@
     qt5.qtx11extras
     libsForQt5.kdelibs4support
     (libsForQt5.kpipewire.overrideAttrs (oldAttrs: {
-      version = "unstable-2023-03-28";
+      version = "unstable-2023-07-24";
 
       src = fetchFromGitLab {
         domain = "invent.kde.org";
         owner = "plasma";
         repo = "kpipewire";
-        rev = "ed99b94be40bd8c5b7b2a2f17d0622f11b2ab7fb";
-        hash = "sha256-KhmhlH7gaFGrvPaB3voQ57CKutnw5DlLOz7gy/3Mzms=";
+        rev = "c21da54fef3d0c1f35c73c57e8a6a61b053b07bf";
+        sha256 = "sha256-xcuSWURiyY9iuayeY9w6G59UJTbYXyPWGg8x+EiXNsY=";
       };
     }))
   ];
