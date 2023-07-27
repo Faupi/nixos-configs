acd9dd90ede42a72f43040c48f76690dec9129e5
Faupi <matej.sp583@gmail.com>
2023-07-27 18:30:20 +0200

    Added 1Password extension for Firefox

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -22,6 +22,11 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
           # TODO: Add a synced configuration
         };
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          # 1Password
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+        };
       };
     };
     preferences = {
