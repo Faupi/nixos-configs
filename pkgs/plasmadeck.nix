73acac48759ce573b5e1488c593f7bf80deebeb7
Faupi <matej.sp583@gmail.com>
2023-07-28 14:16:11 +0200

    Added installation script for PlasmaDeck

--- pkgs/plasmadeck.nix
+++ pkgs/plasmadeck.nix
@@ -11,4 +11,16 @@
     rev = "b03f24240b4d7450524e1731aa19b8884df9c864";
     sha256 = "sha256-GZanIQdGiLp2Luosr+LwED1xirgMVCHbU3bTRsNMLiU=";
   };
+
+  # NOTE: We don't install any wallpapers
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share
+    mv color-schemes $out/share/color-schemes
+    mv plasma $out/share/plasma
+    mv aurorae $out/share/aurorae
+
+    runHook postInstall
+  '';
 }
