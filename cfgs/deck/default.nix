6f52d4e3861420e5db6918feaf6f63bdad96361e
Faupi <matej.sp583@gmail.com>
2023-07-30 17:57:47 +0200

    removed ckb-next

--- cfgs/deck/default.nix
+++ cfgs/deck/default.nix
@@ -18,9 +18,6 @@
   
   services.openssh.enable = true;  # TODO: Remove when installed
 
-  # Enable ckb-next for Corsair peripheral support
-  hardware.ckb-next.enable = true;
-
   # Display
   services.xserver = {
     enable = true;
