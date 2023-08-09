032c32d99337074af292cf7d010befedd5b8a55d
Faupi <matej.sp583@gmail.com>
2023-08-09 11:56:35 +0200

    dumb

--- modules/_1password.nix
+++ modules/_1password.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 {
   programs._1password.enable = true;
-  programs._1password-gui.enable = true;
-  polkitPolicyOwners = [ "faupi" ];  # TODO: Create config
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "faupi" ];  # TODO: Create config
+  };
 }
