e401835cd9d35457ddec78a98a00c31b808c3adb
Faupi <matej.sp583@gmail.com>
2023-08-30 09:18:28 +0200

    Added docking station reset quirk

--- cfgs/base/quirks.nix
+++ cfgs/base/quirks.nix
@@ -4,11 +4,14 @@
   # - 0x1b1c:0x0a6b - Corsair HS80 
   #   - Preventing USB reset problem halting Steam Deck docking station USB initialization 
   #   - Disabling volume control since it desyncs with Pipewire and generally is a nuisance with desktop remoting
+  # - 0x28de:0x2001 - Valve Docking Station
+  #   - Prevent USB hang after resetting from bootloader
 
   # USB
   # https://docs.kernel.org/admin-guide/kernel-parameters.html#selector(exact=usbcore.quirks,type=TextQuoteSelector)
   boot.extraModprobeConfig = /* modconf */ ''  
     options usbcore quirks=0x1b1c:0x0a6b:e
+    options usbcore quirks=0x28de:0x2001:e
   '';
 
   # libinput
