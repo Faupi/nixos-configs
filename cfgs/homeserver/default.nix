77116ae2a564a0a7c11c3b1b0c1c4a9e98ad8c7f
Faupi <matej.sp583@gmail.com>
2023-09-15 23:53:15 +0200

    Proper nixremote user setup for builder

--- cfgs/homeserver/default.nix
+++ cfgs/homeserver/default.nix
@@ -13,6 +13,15 @@
     "nixremote"  # Builder user
   ];
 
+  users.users.nixremote = {
+    isSystemUser = true;
+    createHome = true;
+    homeMode = "500";
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZC7qNbk9YSvZB73i1BXsBaJnZe+NPNeqjngRZLvqo1Ukkm9an5Oj36I9xOQ6nz4va4njdNO2wtcrxaZY34pnft2+QLMCKhANFh5XXkoSJP8dVslSrrqc6ww/rnExG5aw/xKlihB/BrDWP/i4TBqZrzzyL5M4i5sfVlHcNfCUlAZLm/dC9ql1JUMRmg37/jNzF52gCF7SnHQQJu3TcYlMkZsxitHhqrGIc8WR+yroMkY+hKnzxY72ybILPVQQvOlrcX9ne8AQ3yr3uJbbK+N7bMSx+v8UhEm4NQXOCZmgi6jT/rqwFOwKMiGx40NQBpTEZZDBecEsaI8ER1RujFEuREgOCEE0xD9jTzv2XkG7yRylENQ30zJuta8NjrFcIfadmT/08paYD1t0E/g3RqZqfBgkgFlyL3iHBg8Eq+de7dOwjbYfzW+1xyEh9DUNXGYGUGT5N6Wyfj3PrprXQ4cWPWchlsVsEkgY8Ci5EH7831EdqOT7L1e+1OzIDfuNJ4Hk= root@deck"
+    ];
+  };
+
   virtualisation.vmVariant = {
     virtualisation = {
       memorySize = 8192;
