5aa75f38682c13765476fd570001578ad4f8a533
Faupi <matej.sp583@gmail.com>
2023-09-20 17:34:09 +0200

    1Password SSH

--- modules/_1password.nix
+++ modules/_1password.nix
@@ -2,6 +2,8 @@
 {
   # Set up 1Password GUI with CLI integration
   # NOTE: Still need to enable "Security > Unlock system using authentication service" and "Developer > CLI integration"
+  #       - plus SSH agent
+  # TODO: Add full configuration for this shiz
 
   security.polkit.enable = true;
   programs._1password.enable = true;
@@ -9,4 +11,9 @@
     enable = true;
     polkitPolicyOwners = [ "faupi" ];  # TODO: Create config
   };
+
+  programs.ssh.extraConfig = ''
+    Host *
+      IdentityAgent ~/.1password/agent.sock
+  '';
 }
