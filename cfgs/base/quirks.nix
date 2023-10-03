d5cc80c340b1db4e78dc81ba446caa5db77428b5
Faupi <matej.sp583@gmail.com>
2023-10-03 14:10:04 +0200

    Finally resolved the HS80 hang issue I think

--- cfgs/base/quirks.nix
+++ cfgs/base/quirks.nix
@@ -1,18 +1,14 @@
 { config, pkgs, lib, ... }:
 {
   # List of quirks:
-  # TODO: Update
   # - 0x1b1c:0x0a6b - Corsair HS80 
-  #   - Preventing USB reset problem halting Steam Deck docking station USB initialization 
+  #   - Preventing USB halting Steam Deck docking station during USB initialization (HS80 doesn't like Linux's LPM apparently)
   #   - Disabling volume control since it desyncs with Pipewire and generally is a nuisance with desktop remoting
-  # - 0x28de:0x2001 - Valve Docking Station
-  #   - Prevent USB hang after resetting from bootloader
 
   # USB
   # https://docs.kernel.org/admin-guide/kernel-parameters.html#selector(exact=usbcore.quirks,type=TextQuoteSelector)
   boot.extraModprobeConfig = /* modconf */ ''  
-    options usbcore quirks=0x1b1c:0x0a6b:ek
-    options usbcore quirks=0x28de:0x2001:e
+    options usbcore quirks=0x1b1c:0x0a6b:k
   '';
 
   # libinput
