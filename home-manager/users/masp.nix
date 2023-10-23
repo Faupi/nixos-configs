9da4e81f957b9b8bceecaf66b4dd9c4e83c1cf5a
Faupi <matej.sp583@gmail.com>
2023-10-23 13:48:43 +0200

    Fixed VSC on HM

--- home-manager/users/masp.nix
+++ home-manager/users/masp.nix
@@ -1,6 +1,7 @@
-{ lib, pkgs, fop-utils, ... }@args: {
+{ config, lib, pkgs, ... }: {
   programs = {
-    vscode.package = pkgs.vscodium-fhs;
+    # TODO: Check if switching to non-FHS would be better overall
+    vscode.package = pkgs.vscodium;
 
     _1password = {
       enable = true;
