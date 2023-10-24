d50ac673dec40d1a889a1be44d97ef4501fe8557
Faupi <matej.sp583@gmail.com>
2023-10-24 13:30:02 +0200

    Added homereload to shell

--- home-manager/users/shared/shell/functions.sh
+++ home-manager/users/shared/shell/functions.sh
@@ -1,3 +1,6 @@
 nixreload() {
   sudo nixos-rebuild switch --flake github:Faupi/nixos-configs --refresh --no-update-lock-file "$@"
 }
+homereload() {
+  home-manager switch --flake github:Faupi/nixos-configs "$@"
+}
