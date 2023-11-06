624e5efebb26360bfc7f4f13c6797d02631a70db
Faupi <matej.sp583@gmail.com>
2023-11-06 09:57:26 +0100

    added backup arg to homereload

--- home-manager/cfgs/shared/shell-lib/functions.sh
+++ home-manager/cfgs/shared/shell-lib/functions.sh
@@ -2,5 +2,5 @@ nixreload() {
   sudo nixos-rebuild switch --flake github:Faupi/nixos-configs --refresh --no-update-lock-file "$@"
 }
 homereload() {
-  home-manager switch --flake github:Faupi/nixos-configs --refresh "$@"
+  home-manager switch --flake github:Faupi/nixos-configs --refresh -b backup "$@"
 }
