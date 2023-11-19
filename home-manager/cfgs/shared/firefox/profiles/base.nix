e3f053c9e4326693e09b41a1fadc83173ee1fa1e
Faupi <matej.sp583@gmail.com>
2023-11-19 15:43:00 +0100

    Fuck youtube

--- home-manager/cfgs/shared/firefox/profiles/base.nix
+++ home-manager/cfgs/shared/firefox/profiles/base.nix
@@ -96,7 +96,7 @@ with lib; {
   extensions = with pkgs.nur.repos.rycee.firefox-addons; [
     # TODO: Add CopyTables?
     plasma-integration
-    ublock-origin-lite
+    ublock-origin
     darkreader # TODO: Link config
     duckduckgo-privacy-essentials
     brandon1024-find # Regex find
