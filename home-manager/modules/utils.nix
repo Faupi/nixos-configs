c061e8ad12f61044057565f2e7d5267ddc8549e9
Faupi <matej.sp583@gmail.com>
2023-12-11 14:02:13 +0100

    Added global autostart delay

--- home-manager/modules/utils.nix
+++ home-manager/modules/utils.nix
@@ -5,8 +5,14 @@
     # See https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/make-desktopitem/default.nix for arguments
     # Shitty workaround but for the checks it's worth it™
     makeAutostartItem = { name, ... }@args:
+      let
+        renderedArgs = args // {
+          # Add a 5 second delay because of task icon resolution loading problems on KDE
+          exec = ''sh -c "sleep 5 && ${args.exec}"'';
+        };
+      in
       {
-        source = "${pkgs.makeDesktopItem args}/share/applications/${name}.desktop";
+        source = "${pkgs.makeDesktopItem renderedArgs}/share/applications/${name}.desktop";
         target = ".config/autostart/${name}.desktop";
       };
   };
