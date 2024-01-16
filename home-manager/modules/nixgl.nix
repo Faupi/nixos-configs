aa86c7b75a4db9c7237cea83b9b9e6bd383c1250
Faupi <matej.sp583@gmail.com>
2024-01-16 15:08:31 +0100

    Expose used NixGL package in the user environment

--- home-manager/modules/nixgl.nix
+++ home-manager/modules/nixgl.nix
@@ -1,4 +1,5 @@
 { config, lib, pkgs, ... }:
+with lib;
 {
   options = {
     nixGLPackage = lib.mkOption {
@@ -37,38 +38,43 @@
     };
   };
 
-  config.lib.nixgl = {
-    # Wrap the package's binaries with nixGL, while preserving the rest of the outputs and derivation attributes.
-    # Usage: `X.package = config.lib.nixgl.wrapPackage pkgs.X`
-    # https://github.com/nix-community/nixGL/issues/114#issuecomment-1585323281
-    wrapPackage = pkg:
-      if config.nixGLPackage == null then
-        pkg
-      else
-        (pkg.overrideAttrs (old: {
-          name = "nixGL-${pkg.name}";
-          buildCommand = ''
-            set -eo pipefail
+  config = {
+    # Add appropriate nixGL package to user's environment
+    home.packages = (mkIf (config.nixGLPackage != null) [ config.nixGLPackage ]);
 
-            ${
-              pkgs.lib.concatStringsSep "\n" (map (outputName: ''
-                echo "Copying output ${outputName}"
-                set -x
-                cp -rs --no-preserve=mode "${pkg.${outputName}}" "''$${outputName}"
-                set +x
-              '') 
-              (old.outputs or [ "out" ]))
-            }
+    lib.nixgl = {
+      # Wrap the package's binaries with nixGL, while preserving the rest of the outputs and derivation attributes.
+      # Usage: `X.package = config.lib.nixgl.wrapPackage pkgs.X`
+      # https://github.com/nix-community/nixGL/issues/114#issuecomment-1585323281
+      wrapPackage = pkg:
+        if config.nixGLPackage == null then
+          pkg
+        else
+          (pkg.overrideAttrs (old: {
+            name = "nixGL-${pkg.name}";
+            buildCommand = ''
+              set -eo pipefail
 
-            rm -rf $out/bin/*
-            shopt -s nullglob # Prevent loop from running if no files
-            for file in ${pkg.out}/bin/*; do
-              echo "#!${lib.getExe pkgs.bash}" > "$out/bin/$(basename $file)"
-              echo "exec -a \"\$0\" ${lib.getExe config.nixGLPackage} $file \"\$@\"" >> "$out/bin/$(basename $file)"
-              chmod +x "$out/bin/$(basename $file)"
-            done
-            shopt -u nullglob # Revert nullglob back to its normal default state
-          '';
-        }));
+              ${
+                pkgs.lib.concatStringsSep "\n" (map (outputName: ''
+                  echo "Copying output ${outputName}"
+                  set -x
+                  cp -rs --no-preserve=mode "${pkg.${outputName}}" "''$${outputName}"
+                  set +x
+                '') 
+                (old.outputs or [ "out" ]))
+              }
+
+              rm -rf $out/bin/*
+              shopt -s nullglob # Prevent loop from running if no files
+              for file in ${pkg.out}/bin/*; do
+                echo "#!${lib.getExe pkgs.bash}" > "$out/bin/$(basename $file)"
+                echo "exec -a \"\$0\" ${lib.getExe config.nixGLPackage} $file \"\$@\"" >> "$out/bin/$(basename $file)"
+                chmod +x "$out/bin/$(basename $file)"
+              done
+              shopt -u nullglob # Revert nullglob back to its normal default state
+            '';
+          }));
+    };
   };
 }
