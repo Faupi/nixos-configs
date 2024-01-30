811370a96940dd347527929d72d4d3bc535900ec
Faupi <matej.sp583@gmail.com>
2024-01-30 11:30:42 +0100

    no eval cache for HM reload

--- home-manager/cfgs/shared/shell-lib/functions.sh
+++ home-manager/cfgs/shared/shell-lib/functions.sh
@@ -2,5 +2,5 @@ nixreload() {
   sudo nixos-rebuild switch --flake github:Faupi/nixos-configs --refresh --no-update-lock-file "$@"
 }
 homereload() {
-  home-manager switch --flake github:Faupi/nixos-configs --refresh -b backup "$@"
+  home-manager switch --flake github:Faupi/nixos-configs --refresh -b backup --option eval-cache false "$@"
 }
