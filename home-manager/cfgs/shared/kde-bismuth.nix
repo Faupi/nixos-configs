63ffdb7c59f09c5562dec95ac91aa7895bb5ee66
Faupi <matej.sp583@gmail.com>
2024-02-02 02:52:58 +0100

    Initial Bismuth config

--- home-manager/cfgs/shared/kde-bismuth.nix
+++ home-manager/cfgs/shared/kde-bismuth.nix
@@ -1,5 +1,63 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
     kde-bismuth # Custom packaging -> pkgs/kde-bismuth/default.nix
   ];
+
+  # TODO: Add kwin rule for minimal window size (kwin rules need a proper module home-manager/modules/kde-plasma/config-kwin.nix)
+
+  programs.plasma.configFile.kwinrc = {
+    Plugins.bismuthEnabled = true; # Auto-enable
+
+    Script-bismuth = {
+      enableTileLayout = true;
+      enableMonocleLayout = true;
+      enableSpiralLayout = false;
+      enableSpreadLayout = false;
+      enableStairLayout = false;
+      enableThreeColumnLayout = false;
+      enableQuarterLayout = false;
+      enableFloatingLayout = false;
+
+      maximizeSoleTile = true;
+      noTileBorder = true;
+      monocleMaximize = true;
+      monocleMinimizeRest = false;
+      layoutPerActivity = true;
+      layoutPerDesktop = true;
+
+      # Defaults because idk
+      # <entry name="(\w+)"(?:.*)$.*\n.*\n\s+<default>(.*)<\/default>
+      # bismuth/src/config/bismuth_config.kcfg
+      ignoreActivity = "";
+      ignoreClass = "yakuake,spectacle,Conky,zoom";
+      ignoreRole = "quake";
+      ignoreScreen = "";
+      ignoreTitle = "";
+      floatingClass = "";
+      floatingTitle = "";
+      floatUtility = true;
+      untileByDragging = true;
+      keepFloatAbove = true;
+      preventMinimize = false;
+      preventProtrusion = true;
+      screenGapLeft = 0;
+      screenGapRight = 0;
+      screenGapTop = 0;
+      screenGapBottom = 0;
+      tileLayoutGap = 0;
+      limitTileWidth = false;
+      newWindowAsMaster = false;
+      experimentalBackend = false;
+    };
+
+    # Focusing rules for good tiling usage
+    Windows = {
+      AutoRaise = true;
+      AutoRaiseInterval = 0;
+      DelayFocusInterval = 0;
+      FocusPolicy = "FocusFollowsMouse";
+      SeparateScreenFocus = true;
+    };
+  };
 }
