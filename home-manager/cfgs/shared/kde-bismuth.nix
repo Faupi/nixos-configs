b2cce196221da1ce18c34c949db0653132e7354a
Faupi <matej.sp583@gmail.com>
2024-02-03 03:01:45 +0100

    Added a default min size kwin rule for bismuth

--- home-manager/cfgs/shared/kde-bismuth.nix
+++ home-manager/cfgs/shared/kde-bismuth.nix
@@ -7,68 +7,84 @@
   # TODO: Add kwin rule for minimal window size (kwin rules need a proper module home-manager/modules/kde-plasma/config-kwin.nix)
   # TODO: Shortcut setup
 
-  programs.plasma.configFile.kwinrc = {
-    Plugins = {
-      bismuthEnabled = true; # Auto-enable Bismuth
-      tileseditorEnabled = false; # Disable the Plasma tiling editor
+  programs.plasma = {
+    kwin.rules = {
+      "01 Bismuth tiling default" = {
+        enable = true;
+        extraConfig = {
+          wmclassmatch = 0; # Class unimportant
+          types = 1; # All normal windows
+
+          # Force minimum size limit
+          minsize = "100,100";
+          minsizerule = 2;
+        };
+      };
     };
 
-    Script-bismuth = {
-      enableTileLayout = true;
-      enableMonocleLayout = true;
-      enableSpiralLayout = false;
-      enableSpreadLayout = false;
-      enableStairLayout = false;
-      enableThreeColumnLayout = false;
-      enableQuarterLayout = false;
-      enableFloatingLayout = false;
+    configFile.kwinrc = {
+      Plugins = {
+        bismuthEnabled = true; # Auto-enable Bismuth
+        tileseditorEnabled = false; # Disable the Plasma tiling editor
+      };
 
-      maximizeSoleTile = true;
-      noTileBorder = true;
-      monocleMaximize = true;
-      monocleMinimizeRest = false;
-      layoutPerActivity = true;
-      layoutPerDesktop = true;
+      Script-bismuth = {
+        enableTileLayout = true;
+        enableMonocleLayout = true;
+        enableSpiralLayout = false;
+        enableSpreadLayout = false;
+        enableStairLayout = false;
+        enableThreeColumnLayout = false;
+        enableQuarterLayout = false;
+        enableFloatingLayout = false;
 
-      # Defaults because idk
-      # <entry name="(\w+)"(?:.*)$.*\n.*\n\s+<default>(.*)<\/default>
-      # bismuth/src/config/bismuth_config.kcfg
-      ignoreActivity = "";
-      ignoreClass = "yakuake,spectacle,Conky,zoom";
-      ignoreRole = "quake";
-      ignoreScreen = "";
-      ignoreTitle = "";
-      floatingClass = "";
-      floatingTitle = "";
-      floatUtility = true;
-      untileByDragging = true;
-      keepFloatAbove = true;
-      preventMinimize = false;
-      preventProtrusion = true;
-      screenGapLeft = 0;
-      screenGapRight = 0;
-      screenGapTop = 0;
-      screenGapBottom = 0;
-      tileLayoutGap = 0;
-      limitTileWidth = false;
-      newWindowAsMaster = false;
-      experimentalBackend = false;
-    };
+        maximizeSoleTile = true;
+        noTileBorder = true;
+        monocleMaximize = true;
+        monocleMinimizeRest = false;
+        layoutPerActivity = true;
+        layoutPerDesktop = true;
 
-    # Focusing rules for good tiling usage
-    Windows = {
-      AutoRaise = true;
-      AutoRaiseInterval = 0;
-      DelayFocusInterval = 0;
-      FocusPolicy = "FocusFollowsMouse";
-      NextFocusPrefersMouse = true; # Mouse precedence
+        # Defaults because idk
+        # <entry name="(\w+)"(?:.*)$.*\n.*\n\s+<default>(.*)<\/default>
+        # bismuth/src/config/bismuth_config.kcfg
+        ignoreActivity = "";
+        ignoreClass = "yakuake,spectacle,Conky,zoom";
+        ignoreRole = "quake";
+        ignoreScreen = "";
+        ignoreTitle = "";
+        floatingClass = "";
+        floatingTitle = "";
+        floatUtility = true;
+        untileByDragging = true;
+        keepFloatAbove = true;
+        preventMinimize = false;
+        preventProtrusion = true;
+        screenGapLeft = 0;
+        screenGapRight = 0;
+        screenGapTop = 0;
+        screenGapBottom = 0;
+        tileLayoutGap = 0;
+        limitTileWidth = false;
+        newWindowAsMaster = false;
+        experimentalBackend = false;
+      };
 
-      # Open floating windows always in the center
-      OpenGLIsUnsafe = true; # Restoring position
-      Placement = "Centered";
+      # Focusing rules for good tiling usage
+      Windows = {
+        AutoRaise = true;
+        AutoRaiseInterval = 0;
+        DelayFocusInterval = 0;
+        FocusPolicy = "FocusFollowsMouse";
+        NextFocusPrefersMouse = true; # Mouse precedence
 
-      # Multi-screen
-      SeparateScreenFocus = true;
+        # Open floating windows always in the center
+        OpenGLIsUnsafe = true; # Restoring position
+        Placement = "Centered";
+
+        # Multi-screen
+        SeparateScreenFocus = true;
+      };
     };
   };
 }
