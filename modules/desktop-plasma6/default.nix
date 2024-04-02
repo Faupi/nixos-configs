b3cdf54d15cbf2fdbdbf85406a8c58946d983c03
Faupi <matej.sp583@gmail.com>
2024-04-02 14:02:35 +0200

    Added Plasma6 module

---
+++ modules/desktop-plasma6/default.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, lib, ... }:
+with lib;
+let cfg = config.my.plasma6;
+in {
+  options.my.plasma6 = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      # Display
+      services.xserver = {
+        enable = true;
+        excludePackages = [ pkgs.xterm ];
+      };
+
+      xdg.portal = {
+        extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+        xdgOpenUsePortal = true;
+      };
+
+      # Desktop
+      services.desktopManager.plasma6 = {
+        enable = true;
+        notoPackage = pkgs.noto-fonts;
+      };
+      environment.plasma6.excludePackages = with pkgs.libsForQt5; [
+        elisa
+        oxygen
+        khelpcenter
+        print-manager
+      ];
+      environment.sessionVariables = {
+        GTK_USE_PORTAL = "1";
+      };
+
+      programs.dconf.enable = true;
+    })
+  ];
+}
