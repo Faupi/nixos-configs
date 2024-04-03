9f7539e3ab74c79ce1d2cad6190e23282b04b73b
Faupi <matej.sp583@gmail.com>
2024-04-03 15:14:19 +0200

    Patched up automatic ssh keygen

--- cfgs/base/nixremote-key.sh
+++ cfgs/base/nixremote-key.sh
@@ -5,10 +5,12 @@ keyDir="/root/.ssh"
 keyName="nixremote"
 keyPath="$keyDir/$keyName"
 
+set -o errexit
+
 if [[ ! -e "$keyPath" ]]; then
   echo "NixRemote SSH key not found, generating a new one.."
   mkdir -p -m600 "$keyDir"
-  ssh-keygen -t rsa -f "$keyPath" -N "" -q
+  ssh-keygen -t rsa -f "$keyPath" -q -N \"\"
 
   # Print-out
   echo -e "\033[1;34mPublic NixRemote SSH key:"
