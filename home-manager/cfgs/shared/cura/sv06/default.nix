bdfde96d846a0d0fbc7051e8c6a2784e186457dc
Faupi <matej.sp583@gmail.com>
2024-04-10 01:34:44 +0200

    Added printer config to cura

--- home-manager/cfgs/shared/cura/sv06/default.nix
+++ home-manager/cfgs/shared/cura/sv06/default.nix
@@ -1,15 +1,21 @@
 { localPath, ... }:
 {
   home.file = {
-    "SV06 machine" = {
+    "SV06 machine custom" = {
       target = "${localPath}/quality_changes/sovol_sv06_faupi.inst.cfg";
-      source = ./faupi.inst.cfg;
+      source = ./quality_changes/faupi.inst.cfg;
       mutable = true;
       force = true;
     };
-    "SV06 extruder 0" = {
+    "SV06 extruder 0 custom" = {
       target = "${localPath}/quality_changes/sovol_planetary_extruder_0_%232_faupi.inst.cfg";
-      source = ./faupi_extruder.inst.cfg;
+      source = ./quality_changes/faupi_extruder.inst.cfg;
+      mutable = true;
+      force = true;
+    };
+    "SV06 machine" = {
+      target = "${localPath}/definition_changes/Sovol+SV06_settings.inst.cfg";
+      source = ./definition_changes/Sovol+SV06_settings.inst.cfg;
       mutable = true;
       force = true;
     };
