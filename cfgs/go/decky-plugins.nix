156e2a4dc807b8c0e0a355281c86bb657ed76cb2
Faupi <matej.sp583@gmail.com>
2024-04-25 18:39:17 +0200

    Fixed LeGo theme persistency

--- cfgs/go/decky-plugins.nix
+++ cfgs/go/decky-plugins.nix
@@ -1,5 +1,6 @@
 # TODO: I swear to god rework this into a module or I'll cry
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+with lib;
 let
   mainUser = "faupi";
   dataPath = config.jovian.decky-loader.stateDir;
@@ -18,13 +19,25 @@ let
     rev = "bcf7333bad802846fb4e61fed8e70d6e13e8112d";
     sha256 = "1d9azmjsjmv6d2w8d7f695v03mq8gx039wyhz25jbk4v1yd0vr7y";
   };
+  legion-go-theme-config = generators.toJSON { } {
+    active = true;
+    "Apply" = "Xbox/Legion Go";
+    "L is Select" = "No";
+    "L is Start" = "No";
+  };
 in
 {
   system.activationScripts.installDeckyPlugins = ''
-    ln -snf "${hhd-decky}" "${pluginPath}/hhd-decky"
+    # HHD CONFIG BINDING WORKAROUND
+    # NOTE: Decky needs to be root, hhd is set up under the actual user -> configs need to be linked
     mkdir -p "/home/root/.config"
     ln -snf "/home/${mainUser}/.config/hhd" "/home/${config.jovian.decky-loader.user}/.config/hhd"
 
+    # PLUGINS
+    ln -snf "${hhd-decky}" "${pluginPath}/hhd-decky"
+
+    # THEMES
     cp -Tarf "${legion-go-theme}" "${themesPath}/legion-go"
+    cp -Taf "${legion-go-theme-config}" "${themesPath}/legion-go/config_ROOT.json"
   '';
 }
