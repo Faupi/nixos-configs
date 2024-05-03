405892023d0267cdf2ddb5124e3c730ef9b86369
Faupi <matej.sp583@gmail.com>
2024-05-03 09:31:22 +0200

    flake regions

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   # - Impermanence
   # - Switch default stable to default unstable for same lib across all systems - pkgs should have a stable overlay
 
+  #region Inputs
   inputs = rec {
     # Base
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
@@ -97,6 +98,7 @@
     in
     with lib;
     let
+      #region Helpers
       # Helper with default nixpkgs configuration
       defaultNixpkgsConfig = system:
         { extraOverlays ? [ ]
@@ -215,6 +217,7 @@
         };
     in
     {
+      #region Overlays
       overlays = {
         # Local custom packages
         default = final: prev: (
@@ -321,6 +324,7 @@
 
       };
 
+      #region Users
       # Base home configs compatible with NixOS configs
       # TODO: Add custom check for homeUsers
       # TODO: Make configs automatically require their needed modules (spicetify, plasma, etc.)
@@ -378,6 +382,7 @@
         })
       ];
 
+      #region Homes
       # Home manager configurations used by home-manager
       homeConfigurations = fop-utils.recursiveMerge [
         (mkHomeConfiguration "masp" {
@@ -395,6 +400,7 @@
         })
       ];
 
+      #region Systems
       # System configurations
       nixosConfigurations = fop-utils.recursiveMerge [
         (mkSystem "homeserver" {
