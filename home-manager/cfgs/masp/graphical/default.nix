1224ed40b285cfb9f5bdfe063f77f1bdb69b3916
Faupi <matej.sp583@gmail.com>
2024-05-20 15:43:43 +0200

    chromium gaming

--- home-manager/cfgs/masp/graphical/default.nix
+++ home-manager/cfgs/masp/graphical/default.nix
@@ -3,10 +3,12 @@ with lib;
 {
   home.packages = with pkgs; [
     (config.lib.nixgl.wrapPackage krita)
-    (config.lib.nixgl.wrapPackage epiphany)
     (config.lib.nixgl.wrapPackage moonlight-qt)
     localsend
     haruna # Video player
+
+    (config.lib.nixgl.wrapPackage epiphany)
+    chromium
     postman
   ];
 
