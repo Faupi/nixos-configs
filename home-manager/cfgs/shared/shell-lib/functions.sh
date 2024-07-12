6f52ed3ebbca1b01b954ddd913e2882ff2c217ec
Faupi <matej.sp583@gmail.com>
2024-07-12 09:38:07 +0200

    Updated zsh utility functions with rebuild operations and jsonToNix

--- home-manager/cfgs/shared/shell-lib/functions.sh
+++ home-manager/cfgs/shared/shell-lib/functions.sh
@@ -1,6 +1,27 @@
 nixreload() {
-  sudo nixos-rebuild switch --flake github:Faupi/nixos-configs --refresh --no-update-lock-file "$@"
+  if [[ "$#" -lt 1 ]]; then
+    echo "Usage: $0 <operation> [options...]"
+    return
+  fi
+  action="$1"
+  shift 1
+  sudo nixos-rebuild $action --flake github:Faupi/nixos-configs --refresh --no-update-lock-file "$@"
 }
 homereload() {
-  home-manager switch --flake github:Faupi/nixos-configs --refresh -b backup --option eval-cache false "$@"
+  if [[ "$#" -lt 1 ]]; then
+    echo "Usage: $0 <operation> [options...]"
+    return
+  fi
+  action="$1"
+  shift 1
+  home-manager $action --flake github:Faupi/nixos-configs --refresh -b backup --option eval-cache false "$@"
+}
+jsonToNix() {
+  if [[ "$#" -lt 1 ]]; then
+    echo "Usage: $0 <path>"
+    return
+  fi
+  file="$1"
+  shift 1
+  nix eval --impure --expr "builtins.fromJSON (builtins.readFile \"$file\")"
 }
