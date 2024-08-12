f7d0d091faa1e090e54ef062d043548d1f001212
Faupi <matej.sp583@gmail.com>
2024-08-13 01:57:40 +0200

    adjustor update

--- pkgs/adjustor.nix
+++ pkgs/adjustor.nix
@@ -4,20 +4,23 @@
 }:
 python3.pkgs.buildPythonApplication rec {
   pname = "adjustor";
-  version = "2.1.5";
+  version = "3.4.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "hhd-dev";
     repo = "adjustor";
     rev = "v${version}";
-    hash = "sha256-KgeK2lC3b9VbjYmy7rD/ImODWDDx3pWIzWgNnSLJeDs=";
+    hash = "sha256-/fdarz2rfDfwgjE4VtbtBTSouc6Ttx/Dlw7QxttMxZ0=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
     setuptools
     rich
     pyroute2
+    fuse
+    pygobject3
+    dbus-python
   ];
 
   # This package doesn't have upstream tests.
