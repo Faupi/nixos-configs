1396d8498fab1181b1b42d36348d1676bb69f49a
Faupi <matej.sp583@gmail.com>
2024-08-24 01:58:37 +0200

    mjpg config

--- nixos/modules/octoprint/default.nix
+++ nixos/modules/octoprint/default.nix
@@ -8,6 +8,8 @@
 
   services.mjpg-streamer = {
     enable = true;
+    inputPlugin = "input_uvc.so --device /dev/video0 --resolution 1280x720 --fps 30";
+    outputPlugin = "output_http.so -w @www@ -n -p 5050";
   };
 
   services.octoprint = {
