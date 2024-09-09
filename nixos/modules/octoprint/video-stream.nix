6bf765c5da9731d0409b2f979ae1eea1a63b49b2
Faupi <matej.sp583@gmail.com>
2024-09-09 23:06:19 +0200

    octoprint video finetune

--- nixos/modules/octoprint/video-stream.nix
+++ nixos/modules/octoprint/video-stream.nix
@@ -5,7 +5,7 @@ in
 {
   services.mjpg-streamer = {
     enable = true;
-    inputPlugin = "input_uvc.so --device /dev/video0 --resolution 1280x720 --fps 30 -wb 3000";
+    inputPlugin = "input_uvc.so --device /dev/video0 --resolution 1280x720 --fps 30 -wb 4000 -bk 1 -ex 1000 -gain 255 -cagc auto -sh 100";
     outputPlugin = "output_http.so -w @www@ -n -p ${toString p}";
   };
   networking.firewall = {
