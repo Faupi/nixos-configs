431dcde7c38f26d13135c8c768a73e55bf105bb6
Faupi <matej.sp583@gmail.com>
2024-10-01 02:38:49 +0200

    Temp switch to Jovian fork

--- flake.nix
+++ flake.nix
@@ -40,7 +40,8 @@
 
     # Steamdeck wrappers
     jovian = {
-      url = "github:Jovian-Experiments/Jovian-NixOS";
+      # TODO: Switch to upstream once https://github.com/Jovian-Experiments/Jovian-NixOS/pull/415 is merged
+      url = "github:faupi/Jovian-NixOS";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
