270d139dc24c9a5cae28118e9b62bbe2581f452e
Faupi <matej.sp583@gmail.com>
2024-10-03 09:10:32 +0200

    Firefox profile sync

--- home-manager/cfgs/shared/firefox-profiles/faupi.nix
+++ home-manager/cfgs/shared/firefox-profiles/faupi.nix
@@ -3,11 +3,6 @@ fop-utils.recursiveMerge [
   (import ./base.nix args)
   {
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-      ublock-origin
-      sponsorblock # TODO: Link ID thru sops
-      istilldontcareaboutcookies # Automatic cookie denial
-      onepassword-password-manager
-
       enhancer-for-youtube # Fuck the new layout changes seriously
       protondb-for-steam
       steam-database
