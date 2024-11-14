8aa1998ab04093fbc4a812fa6a884c2bf7127836
Faupi <matej.sp583@gmail.com>
2024-11-14 16:05:55 +0100

    homeserver substitute

--- nixos/cfgs/LT-masp/default.nix
+++ nixos/cfgs/LT-masp/default.nix
@@ -18,6 +18,11 @@
     dates = "weekly";
   };
 
+  # TODO: Propagate across every device EXCEPT for the same builder - probably needs to be in flake.nix
+  nix.settings.substituters = [
+    "ssh-ng://nixremote@homeserver.local"
+  ];
+
   my = {
     plasma6.enable = true;
     localsend.enable = true;
