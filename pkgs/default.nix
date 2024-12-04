b193ab5824265cabac7a892c3b611c76f6156165
Faupi <matej.sp583@gmail.com>
2024-12-04 10:42:53 +0100

    oops, removed stale alvr reference

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -12,7 +12,6 @@ rec {
 
   adjustor = pkgs.callPackage ./adjustor.nix { };
 
-  alvr = pkgs.callPackage ./alvr { };
   css-loader-desktop = pkgs.callPackage ./css-loader-desktop { };
   cura = pkgs.callPackage ./cura.nix { };
   minecraft-server-fabric_1_20_4 = pkgs.callPackage ./minecraft-server-fabric_1_20_4.nix { };
