f2e6c9d27c61aaebc1398da83ce437c3866724a9
Faupi <matej.sp583@gmail.com>
2024-12-20 09:41:48 +0100

    Fixed OpenVPN-indicator build

--- pkgs/openvpn3-indicator/default.nix
+++ pkgs/openvpn3-indicator/default.nix
@@ -1,19 +1,23 @@
-{ fetchFromGitHub
-, python3Packages
-, libayatana-appindicator
-, openvpn3
+# When you spend multiple hours to find out that you just need a setup.py...
+# Anyway most of this was fixed thanks to the existing package on https://github.com/nix-community/nur-combined/blob/master/repos/mloeper/pkgs/openvpn3-indicator/default.nix
+
+{ desktop-file-utils
+, fetchFromGitHub
 , gnumake
 , gnused
-, zip
-, desktop-file-utils
-, shared-mime-info
-, gtk3
 , gobject-introspection
-, json-glib
+, libappindicator-gtk3
+, libayatana-appindicator
+, openvpn3
+, python3Packages
+, shared-mime-info
+, wrapGAppsHook3
+, zip
 }:
 python3Packages.buildPythonApplication {
   pname = "openvpn3-indicator";
   version = "unstable-20241010";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "OpenVPN";
@@ -21,6 +25,9 @@ python3Packages.buildPythonApplication {
     rev = "bc871e0fcbf3b549939f302d1475f2edb9d2e30d";
     hash = "sha256-A9pupZbQoV1m6dy54n4DmZUohaCqCM15uLa50D1D6MI=";
   };
+  patches = [
+    ./add-setup-py.patch
+  ];
 
   postPatch = ''
     echo "Replace build targets"
@@ -37,21 +44,20 @@ python3Packages.buildPythonApplication {
     desktop-file-utils
     shared-mime-info
     gobject-introspection
+    wrapGAppsHook3
   ];
-
   buildInputs = [
-    openvpn3
+    libappindicator-gtk3
     libayatana-appindicator
-    json-glib
-    gtk3
   ];
 
   propagatedBuildInputs = with python3Packages; [
-    dbus-python
+    setuptools
+
     secretstorage
-    setproctitle
+    dbus-python
     pygobject3
+    setproctitle
+    openvpn3
   ];
-  pyproject = false;
-  dontWrapGApps = true;
 }
