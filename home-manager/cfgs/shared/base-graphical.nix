0666981e4f7b47136dd9294b71bbb668a3905c6c
Faupi <matej.sp583@gmail.com>
2025-01-20 10:22:35 +0100

    Added global font config to home manager

--- home-manager/cfgs/shared/base-graphical.nix
+++ home-manager/cfgs/shared/base-graphical.nix
@@ -1,12 +1,24 @@
 { pkgs, config, ... }:
 {
-  # TODO: Figure out why Haruna fails builds under NixGL
   home.packages = (with pkgs; [
+    # TODO: Figure out why Haruna fails builds under NixGL
     haruna
+    noto-fonts
+    twitter-color-emoji
   ])
   ++ (map (x: (config.lib.nixgl.wrapPackage x)) (with pkgs; [
     qpwgraph
     filelight
     krita
   ]));
+
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      serif = [ "Noto Sans" "Twitter Color Emoji" ];
+      sansSerif = [ "Noto Sans" "Twitter Color Emoji" ];
+      emoji = [ "Twitter Color Emoji" ];
+    };
+  };
 }
