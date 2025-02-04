1f50bea8ab4a4deb6d1b462ffe2e52173c027b9c
Faupi <matej.sp583@gmail.com>
2025-02-04 09:19:12 +0100

    Highlight-regex: Update to 2.1.1

--- pkgs/vscode-extensions/highlight-regex/default.nix
+++ pkgs/vscode-extensions/highlight-regex/default.nix
@@ -2,15 +2,15 @@
 , fop-vscode-utils
 }:
 fop-vscode-utils.buildVscodeExtension rec {
-  vscodeExtPublisher = "faupi";
+  vscodeExtPublisher = "MickaelBlet";
   vscodeExtName = "highlight-regex";
-  version = "2.1.0";
+  version = "2.1.1";
 
   src = fetchFromGitHub {
     owner = vscodeExtPublisher;
     repo = vscodeExtName;
-    rev = "d73f3f7664d578f1e9e65ef31e9e4f4dc431c438";
-    sha256 = "sha256-qZSsD/PvUzCfgJ7KTlTyrKoy6yEspAl7zE8NvLHM3V4=";
+    rev = "v${version}";
+    sha256 = "sha256-0RvrU+008XouCiDD5tRqSeg+jvWRRkeLIW0NvVZl6e8=";
   };
 
   sourceRoot = "source";
