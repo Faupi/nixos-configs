42f31eb48138d9fe42d514b61f5e952b0b5cb9f8
Faupi <matej.sp583@gmail.com>
2025-02-12 16:19:48 +0100

    Updated plasma-drawer to 2.0.1

--- pkgs/plasma-drawer.nix
+++ pkgs/plasma-drawer.nix
@@ -6,13 +6,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "plasma-drawer";
-  version = "unstable-20240423";
+  version = "2.0.1";
 
   src = fetchFromGitHub {
     owner = "p-connor";
     repo = pname;
-    rev = "3d511ab0a904e54051761ae3e04d22ed6eab46eb";
-    sha256 = "sha256-C2kiWR7y3LCfoQPZ7RqK2GosdzlsJUR2eB/1HPrwxBE=";
+    rev = "v${version}";
+    sha256 = "sha256-GQ3j78y/0XD1CENgglHsK/3b5783PbzNnNTAQ1rQm5w=";
   };
 
   # idk how I feel about this
