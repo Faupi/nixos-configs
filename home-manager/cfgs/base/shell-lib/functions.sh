26a7704d85f3786a7e6d14ad3bf59bf5b4956f4d
Faupi <matej.sp583@gmail.com>
2025-03-01 03:07:45 +0100

    Added alias for nom reload

--- home-manager/cfgs/base/shell-lib/functions.sh
+++ home-manager/cfgs/base/shell-lib/functions.sh
@@ -7,6 +7,16 @@ nixreload() {
   shift 1
   sudo nixos-rebuild $action --flake github:Faupi/nixos-configs --refresh --no-update-lock-file "$@"
 }
+nomreload() {
+  if [[ "$#" -lt 1 ]]; then
+    echo "Usage: $0 <operation> [options...]"
+    return
+  fi
+  action="$1"
+  shift 1
+  sudo nixos-rebuild $action --flake github:Faupi/nixos-configs --refresh --no-update-lock-file "$@" --verbose --log-format internal-json |& nom --json
+}
+
 homereload() {
   if [[ "$#" -lt 1 ]]; then
     echo "Usage: $0 <operation> [options...]"
@@ -16,6 +26,7 @@ homereload() {
   shift 1
   home-manager $action --flake github:Faupi/nixos-configs --refresh -b backup --option eval-cache false "$@"
 }
+
 json2nix() {
   if [[ "$#" -lt 1 ]]; then
     echo "Usage: $0 <path>"
