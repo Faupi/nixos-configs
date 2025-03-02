d022f7e3cbb511441a680a564731ea9a9b0c684e
Faupi <matej.sp583@gmail.com>
2025-03-02 06:01:58 +0100

    no bind mounts

--- nixos/cfgs/homeserver/containers/octoprint/default.nix
+++ nixos/cfgs/homeserver/containers/octoprint/default.nix
@@ -29,16 +29,6 @@ in
         modifier = "rwm";
       }
     ];
-    bindMounts = {
-      "${printerDev}" = {
-        hostPath = printerDev;
-        isReadOnly = false;
-      };
-      "${camDev}" = {
-        hostPath = camDev;
-        isReadOnly = false;
-      };
-    };
 
     config = {
       system.stateVersion = "25.05";
