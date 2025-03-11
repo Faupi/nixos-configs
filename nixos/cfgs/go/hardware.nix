d683b468fe98940794af1f831c7909fa171282fe
Faupi <matej.sp583@gmail.com>
2025-03-11 04:38:17 +0100

    Initial platform optimization configuration for go

--- nixos/cfgs/go/hardware.nix
+++ nixos/cfgs/go/hardware.nix
@@ -1,6 +1,26 @@
+# TODO: Split off into separate files, as CPU-specific optimizations might also get huge
 { lib, pkgs, ... }:
 {
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nix.settings.system-features = [
+    "benchmark"
+    "big-parallel"
+    "gccarch-znver4"
+    "kvm"
+    "nixos-test"
+  ];
+  nixpkgs.hostPlatform = {
+    system = "x86_64-linux";
+    gcc.arch = "znver4";
+    gcc.tune = "znver4";
+  };
+
+  /* 
+    TODO: Create wrapper for un-optimizing packages, so they build and function - see https://github.com/caffineehacker/nix/blob/main/machines/framework/configuration.nix
+    
+    Failing:
+    - clang
+    - rustc
+  */
 
   boot = {
     kernelModules = [
