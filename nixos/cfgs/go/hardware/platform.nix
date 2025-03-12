af742fcfa0eeb7be5b06984c00a27bf90b871488
Faupi <matej.sp583@gmail.com>
2025-03-12 02:02:18 +0100

    LeGo: Scrapped package overlays for now

--- nixos/cfgs/go/hardware/platform.nix
+++ nixos/cfgs/go/hardware/platform.nix
@@ -1,37 +1,15 @@
-{ ... }: {
+{ pkgs, ... }:
+{
   nix.settings.system-features = [
+    "gccarch-znver4"
     "benchmark"
     "big-parallel"
-    "gccarch-znver4"
     "kvm"
     "nixos-test"
   ];
-  nixpkgs.hostPlatform = {
-    system = "x86_64-linux";
-    gcc.arch = "znver4";
-    gcc.tune = "znver4";
-  };
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   hardware.cpu.amd.updateMicrocode = true;
-
-  /* 
-    TODO: Create wrapper for un-optimizing packages, so they build and function - see https://github.com/caffineehacker/nix/blob/main/machines/framework/configuration.nix
-    
-    Failing:
-    - clang
-    - rustc
-  */
-  nixpkgs.overlays = [
-    (self: super: {
-      # Unoptimize stuff
-
-      clang = super.clang.overrideAttrs (old: {
-        NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or "") + " -march=x86-64 -mtune=generic";
-      });
-
-      rustc = super.clang.overrideAttrs (old: {
-        NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or "") + " -march=x86-64 -mtune=generic";
-      });
-    })
-  ];
 }
