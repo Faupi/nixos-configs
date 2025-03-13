b017ddabc420d6e5b6aa44a61fe8a43aa0f4e7dc
Faupi <matej.sp583@gmail.com>
2025-03-14 00:05:51 +0100

    LeGo: Native-built Mesa init

--- nixos/cfgs/go/hardware/platform.nix
+++ nixos/cfgs/go/hardware/platform.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, fop-utils, ... }:
 {
   nix.settings.system-features = [
     "gccarch-znver4"
@@ -12,4 +12,13 @@
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
   hardware.cpu.amd.updateMicrocode = true;
+
+  nixpkgs.overlays = [
+    (self: super: {
+      # TODO: Strip down Nvidia and Intel drivers potentially - https://github.com/NixOS/nixpkgs/blob/95600680c021743fd87b3e2fe13be7c290e1cac4/pkgs/development/libraries/mesa/default.nix#L46-L88
+      mesa = fop-utils.makeOptLocal super.mesa "-O2";
+      # galliumDrivers = ["llvmpipe" "zink" "radeonsi"]
+      # vulkanDrivers = ["swrast" "amd"]
+    })
+  ];
 }
--- utils.nix
+++ utils.nix
@@ -177,4 +177,37 @@ rec {
       toINI_ = generators.toINI { inherit mkSectionName; };
     in
     toINI_ (flattenAttrs attrs);
+
+  # https://github.com/Nix-QChem/NixOS-QChem/blob/461b04cc9b6fc2173fc37c6c29f1253ed443f92e/lib.nix
+  # Create a stdenv with CPU optimizations
+  makeOptStdenv = pkgs: stdenv: arch: extraCFlags:
+    if arch == null || stdenv.system != "x86_64-linux" then stdenv
+    else
+    # TODO: See if stdenv.withCFlags wouldn't make more sense here - had build issues earlier
+      stdenv.override {
+        name = stdenv.name + "-${arch}";
+
+        # Make sure respective CPU features are set
+        hostPlatform = stdenv.hostPlatform //
+          lib.mapAttrs (p: a: a arch) lib.systems.architectures.predicates;
+
+        # Add additional compiler flags
+        extraAttrs = {
+          mkDerivation = args: (stdenv.mkDerivation args).overrideAttrs (old: {
+            env.NIX_CFLAGS_COMPILE = toString (old.env.NIX_CFLAGS_COMPILE or "")
+              + " -march=${arch} -mtune=${arch} " + extraCFlags;
+          });
+        };
+      };
+
+  makeOptLocal = package: extraCFlags:
+    if package.stdenv.system != "x86_64-linux" then package
+    else
+      (package.overrideAttrs (oldAttrs: {
+        preferLocalBuild = true;
+        allowSubstitutes = false;
+      })).override (old: {
+        # name = package.name + "-native"
+        stdenv = makeOptStdenv super old.stdenv "native" extraCFlags;
+      });
 }
