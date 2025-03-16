c32a3a8cf4935df3c7acb7a84c7d81ebb48b197b
Faupi <matej.sp583@gmail.com>
2025-03-16 01:53:16 +0100

    LeGo: Stripped down Mesa and added Zen and OpenCL to optimized packages

--- nixos/cfgs/go/hardware/platform.nix
+++ nixos/cfgs/go/hardware/platform.nix
@@ -1,4 +1,4 @@
-{ pkgs, fop-utils, ... }:
+{ pkgs, fop-utils, lib, ... }:
 {
   nix.settings.system-features = [
     "gccarch-znver4"
@@ -14,18 +14,29 @@
   hardware.cpu.amd.updateMicrocode = true;
 
   nixpkgs.overlays = [
-    (self: super: {
-      # TODO: Strip down Nvidia and Intel drivers potentially - https://github.com/NixOS/nixpkgs/blob/95600680c021743fd87b3e2fe13be7c290e1cac4/pkgs/development/libraries/mesa/default.nix#L46-L88
-      mesa = fop-utils.makeOptLocal super.mesa "-O2";
-      # galliumDrivers = ["llvmpipe" "zink" "radeonsi"]
-      # vulkanDrivers = ["swrast" "amd"]
+    (self: super:
+      let
+        optimize = package: extraCFlags:
+          if package.stdenv.hostPlatform.system == "x86_64-linux"
+          then fop-utils.mkLocalOptimizedPackage super package ("-O2" + toString extraCFlags)
+          else package;
+      in
+      {
+        # Note - do not override optimization
+        # linuxPackages_zen = pkgs.linuxPackagesFor (fop-utils.mkLocalOptimizedPackage super super.linuxPackages_zen.kernel "");
 
-      /*
-      pkgs.linuxPackages_zen  # TODO: Also add `boot.kernelParams = [ "amd_pstate=active" ];`
-      
-      pkgs.rocmPackages.clr
-      pkgs.rocmPackages.clr.icd
-      */
-    })
+        mesa = ((optimize super.mesa "").override {
+          galliumDrivers = [ "llvmpipe" "zink" "radeonsi" ];
+          vulkanDrivers = [ "swrast" "amd" ];
+        }).overrideAttrs (old: {
+          # Force-remove output, as we don't build it and the output definition is not conditional
+          # https://github.com/NixOS/nixpkgs/pull/371771
+          outputs = lib.remove "spirv2dxil" old.outputs;
+        });
+
+        rocmPackages.clr = optimize super.rocmPackages.clr "";
+
+        zen-browser = optimize super.zen-browser "";
+      })
   ];
 }
