4c86c19b09a0093c627b35c1447da8914f16336b
Faupi <matej.sp583@gmail.com>
2025-03-20 11:14:53 +0100

    Moved home-manager shell config into a separate dir

--- home-manager/cfgs/base/default.nix
+++ home-manager/cfgs/base/default.nix
@@ -1,7 +1,9 @@
-# TODO: Move shell shit into a separate cnfig fucking shared stuff yes
-
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, ... }:
 {
+  imports = [
+    ./shell
+  ];
+
   programs.home-manager.enable = true;
 
   nix = {
@@ -25,7 +27,6 @@
   ];
 
   programs = {
-    # Git
     git = {
       enable = true;
       extraConfig = {
@@ -33,68 +34,5 @@
         core.autocrlf = false; # Fucks with cross-platform usage, seen as a bad default
       };
     };
-
-    #region Shells
-    oh-my-posh = {
-      enable = true;
-      package = with pkgs;
-        oh-my-posh;
-      settings = with builtins; fromJSON (unsafeDiscardStringContext (readFile ./oh-my-posh.json));
-      enableZshIntegration = true;
-    };
-
-    # Fuzzy finder 
-    fzf = {
-      enable = true;
-      package = with pkgs;
-        fzf;
-      enableZshIntegration = true;
-    };
-
-    # Smarter cd
-    zoxide = {
-      enable = true;
-      package = with pkgs;
-        zoxide;
-      enableZshIntegration = true;
-      options = [
-        "--cmd cd"
-      ];
-    };
-
-    # History search
-    atuin = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      nix-direnv.enable = true;
-    };
-
-    zsh = {
-      enable = true;
-      package = with pkgs;
-        zsh;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-
-      initExtra = ''
-        export PATH=${config.home.homeDirectory}/.local/bin:$PATH
-
-        ${lib.getExe config.programs.oh-my-posh.package} disable notice
-        ${lib.getExe (with pkgs; any-nix-shell)} zsh | source /dev/stdin
-        source ${./shell-lib/functions.sh}
-        source ${./shell-lib/zsh-keybinds.zsh}
-        source ${./shell-lib/zsh-nvm.zsh}
-      '';
-
-      sessionVariables = {
-        FZF_DEFAULT_OPTS = "--height=20% --info=inline-right --reverse --header-first";
-      };
-    };
-    #!region
   };
 }
---
+++ home-manager/cfgs/base/shell/default.nix
@@ -0,0 +1,64 @@
+{ config, lib, pkgs, ... }: {
+  programs = {
+    oh-my-posh = {
+      enable = true;
+      package = with pkgs;
+        oh-my-posh;
+      settings = with builtins; fromJSON (unsafeDiscardStringContext (readFile ./oh-my-posh.json));
+      enableZshIntegration = true;
+    };
+
+    # Fuzzy finder 
+    fzf = {
+      enable = true;
+      package = with pkgs;
+        fzf;
+      enableZshIntegration = true;
+    };
+
+    # Smarter cd
+    zoxide = {
+      enable = true;
+      package = with pkgs;
+        zoxide;
+      enableZshIntegration = true;
+      options = [
+        "--cmd cd"
+      ];
+    };
+
+    # History search
+    atuin = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    zsh = {
+      enable = true;
+      package = with pkgs;
+        zsh;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      initExtra = ''
+        export PATH=${config.home.homeDirectory}/.local/bin:$PATH
+
+        ${lib.getExe config.programs.oh-my-posh.package} disable notice
+        ${lib.getExe (with pkgs; any-nix-shell)} zsh | source /dev/stdin
+        source ${./shell-lib/functions.sh}
+        source ${./shell-lib/zsh-keybinds.zsh}
+        source ${./shell-lib/zsh-nvm.zsh}
+      '';
+
+      sessionVariables = {
+        FZF_DEFAULT_OPTS = "--height=20% --info=inline-right --reverse --header-first";
+      };
+    };
+  };
+}
