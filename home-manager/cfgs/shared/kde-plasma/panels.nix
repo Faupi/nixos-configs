cdd7de2e071f96e397ea535a68162a78ffc5583d
Faupi <matej.sp583@gmail.com>
2025-04-21 04:14:06 +0200

    Plasma: Switched to top-bar panel layout

--- home-manager/cfgs/shared/kde-plasma/panels.nix
+++ home-manager/cfgs/shared/kde-plasma/panels.nix
@@ -10,17 +10,21 @@ with lib;
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       plasma-drawer
+      kara
     ];
 
     programs.plasma.panels = [
       {
-        location = "bottom";
-        floating = true;
+        location = "top";
+        floating = false;
         hiding = "none";
         alignment = "center";
+        opacity = "opaque";
         screen = 0;
-        height = 44;
+        height = 28; # REVIEW - 24+4 mostly to give system tray some padding
+        lengthMode = "fill";
         widgets = [
+          /* TODO: Unearth for touch mode
           {
             name = "p-connor.plasma-drawer"; # req: pkgs.plasma-drawer
             config = {
@@ -47,19 +51,159 @@ with lib;
               };
             };
           }
+          */
 
           {
-            name = "org.kde.plasma.pager";
+            kickoff = {
+              icon = cfg.launcherIcon;
+              sidebarPosition = "left";
+              showActionButtonCaptions = false;
+              showButtonsFor.custom = [
+                "suspend"
+                "lock-screen"
+                "logout"
+              ];
+            };
+          }
+
+          {
+            name = "org.dhruv8sh.kara";
             config = {
-              General = {
-                displayedText = "Number";
-                showWindowIcons = "true";
+              # NOTE: lowercase "general"
+              general = {
+                type = 0; # Pills
+                animationDuration = 200; # ms
+              };
+              type1 = {
+                t1activeWidth = 20;
+                t1activeHeight = 10;
+                t1height = 10;
+                t1width = 10;
               };
             };
           }
 
           "org.kde.plasma.panelspacer"
 
+          {
+            systemMonitor = {
+              displayStyle = "org.kde.ksysguard.horizontalbars";
+              title = "System Resources - Processing";
+              showTitle = true;
+              showLegend = true;
+              settings = {
+                "org.kde.ksysguard.horizontalbars/General" = {
+                  rangeAuto = false;
+                  rangeFrom = 0;
+                  rangeTo = 100;
+                };
+              };
+              sensors = [
+                {
+                  name = "cpu/all/usage";
+                  color = sharedOptions.colorCPU;
+                  label = "CPU";
+                }
+                {
+                  name = "gpu/all/usage";
+                  color = sharedOptions.colorGPU;
+                  label = "GPU";
+                }
+              ];
+            };
+          }
+
+          {
+            digitalClock = {
+              time = {
+                showSeconds = "onlyInTooltip";
+                format = "24h";
+              };
+              date = {
+                enable = true;
+                format = { custom = "dd/MM/yyyy"; };
+                position = "besideTime";
+              };
+              calendar = {
+                firstDayOfWeek = "monday";
+                # TODO: Add calendar plugins?
+              };
+            };
+          }
+
+          {
+            systemMonitor = {
+              displayStyle = "org.kde.ksysguard.horizontalbars";
+              title = "System Resources - Memory";
+              showTitle = true;
+              showLegend = true;
+              settings = {
+                "org.kde.ksysguard.horizontalbars/General" = {
+                  rangeAuto = false;
+                  rangeFrom = 0;
+                  rangeTo = 100;
+                };
+              };
+              sensors = [
+                {
+                  name = "memory/physical/usedPercent";
+                  color = sharedOptions.colorMemory;
+                  label = "Memory";
+                }
+                {
+                  name = "memory/swap/usedPercent";
+                  color = sharedOptions.colorSwap;
+                  label = "Swap";
+                }
+              ];
+            };
+          }
+
+          "org.kde.plasma.panelspacer"
+
+          {
+            systemTray = {
+              icons = {
+                spacing = "small";
+                scaleToFit = false;
+              };
+              items = {
+                showAll = false;
+                shown = [
+                  "org.kde.plasma.battery"
+                  "org.kde.plasma.volume"
+                  "org.kde.plasma.keyboardlayout"
+                ];
+                hidden = [
+                  "org.kde.kalendar.contact"
+                  "org.kde.plasma.clipboard"
+                  "org.kde.kscreen"
+                  "org.kde.plasma.devicenotifier"
+                  "org.kde.plasma.brightness"
+                  "org.kde.kdeconnect"
+                  "Discover Notifier_org.kde.DiscoverNotifier"
+                  "Wallet Manager"
+                  "KDE Daemon"
+                  "The KDE Crash Handler"
+                  "touchpad"
+                  "spotify-client"
+                ];
+              };
+            };
+          }
+        ];
+      }
+
+      {
+        location = "bottom";
+        floating = true;
+        hiding = "dodgewindows";
+        alignment = "center";
+        opacity = "translucent";
+        screen = 0;
+        height = 56; # REVIEW
+        lengthMode = "fit";
+        widgets = [
           {
             iconTasks = {
               launchers = [
@@ -90,88 +234,6 @@ with lib;
                 unhideOnAttentionNeeded = true;
                 newTasksAppearOn = "right";
               };
-            };
-          }
-
-          "org.kde.plasma.panelspacer"
-
-          {
-            systemTray = {
-              icons = {
-                spacing = "medium";
-                scaleToFit = false;
-              };
-              items = {
-                showAll = false;
-                shown = [
-                  "org.kde.plasma.battery"
-                  "org.kde.plasma.volume"
-                  "org.kde.plasma.keyboardlayout"
-                ];
-                hidden = [
-                  "org.kde.kalendar.contact"
-                  "org.kde.plasma.clipboard"
-                  "org.kde.kscreen"
-                  "org.kde.plasma.devicenotifier"
-                  "org.kde.plasma.brightness"
-                  "org.kde.kdeconnect"
-                  "Discover Notifier_org.kde.DiscoverNotifier"
-                  "Wallet Manager"
-                  "KDE Daemon"
-                  "The KDE Crash Handler"
-                  "touchpad"
-                  "spotify-client"
-                ];
-              };
-            };
-          }
-
-          {
-            digitalClock = {
-              time = {
-                showSeconds = "onlyInTooltip";
-                format = "24h";
-              };
-              date = {
-                enable = true;
-                format = { custom = "dd/MM/yyyy"; };
-                position = "belowTime";
-              };
-              calendar = {
-                firstDayOfWeek = "monday";
-                # TODO: Add calendar plugins?
-              };
-            };
-          }
-
-          {
-            systemMonitor = {
-              displayStyle = "org.kde.ksysguard.barchart";
-              title = "System Resources";
-              showTitle = true;
-              showLegend = true;
-              sensors = [
-                {
-                  name = "cpu/all/usage";
-                  color = sharedOptions.colorCPU;
-                  label = "CPU";
-                }
-                {
-                  name = "gpu/all/usage";
-                  color = sharedOptions.colorGPU;
-                  label = "GPU";
-                }
-                {
-                  name = "memory/physical/usedPercent";
-                  color = sharedOptions.colorMemory;
-                  label = "Memory";
-                }
-                {
-                  name = "memory/swap/usedPercent";
-                  color = sharedOptions.colorSwap;
-                  label = "Swap";
-                }
-              ];
             };
           }
         ];
