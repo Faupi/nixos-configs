a9dc52fbff140ad6eb68b31fb01bebce0c63bd0b
Faupi <matej.sp583@gmail.com>
2025-05-11 23:00:25 +0200

    Disabled a bunch of things to hopefully resolve the sleep issues

--- nixos/cfgs/go/steam/steamos.nix
+++ nixos/cfgs/go/steam/steamos.nix
@@ -1,6 +1,4 @@
 { ... }: {
-  programs.steam.platformOptimizations.enable = true; # SteamOS tweaks https://github.com/fufexan/nix-gaming/blob/64a949349294543a48b3f946c9fca84332d1398b/modules/platformOptimizations.nix
-
   jovian.steamos = {
     useSteamOSConfig = false; # No automatic enabling of stuff in the steamos module
     enableDefaultCmdlineConfig = false; # Already handled by hardware
