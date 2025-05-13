cf0515869e5b9fbe6e1f3fd75151fc97f39082be
Faupi <matej.sp583@gmail.com>
2025-05-13 10:33:31 +0200

    Swapped Firefox addons around profiles

--- home-manager/cfgs/shared/firefox-profiles/faupi.nix
+++ home-manager/cfgs/shared/firefox-profiles/faupi.nix
@@ -2,10 +2,19 @@
 fop-utils.recursiveMerge [
   (import ./base.nix args)
   {
-    extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+    extensions.packages = (with pkgs.nur.repos.rycee.firefox-addons; [
+      consent-o-matic # Automatically decline cookies
+
       enhancer-for-youtube # Fuck the new layout changes seriously
+      sponsorblock # TODO: Link ID thru sops
+
       protondb-for-steam
       steam-database
-    ];
+
+      refined-github
+      lovely-forks # Shows notable forks on GitHub
+    ]) ++ (with pkgs.nur.repos.bandithedoge.firefoxAddons; [
+      material-icons-for-github
+    ]);
   }
 ]
