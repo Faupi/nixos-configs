105b1aa5e87c94bc3549e3ab5e8e417defec2378
Faupi <matej.sp583@gmail.com>
2025-05-28 10:24:21 +0200

    Generalized node matching for device names

--- nixos/cfgs/shared/audio.nix
+++ nixos/cfgs/shared/audio.nix
@@ -34,7 +34,9 @@ in
           extraConfig =
             let
               # Set name the same as Plasma settings' "Sound > Rename Devices" do
-              setName = customName: nodeName: {
+              setName = customName: matches: {
+                matches = [ ({ "port.monitor" = "!true"; } // matches) ];
+
                 actions = {
                   update-props = lib.attrsets.genAttrs [
                     "alsa.card_name"
@@ -46,35 +48,62 @@ in
                   ]
                     (name: customName);
                 };
-                # REVIEW: Look into more general rules for a device as a whole to avoid having to match specific profiles like `pro-input-0` - use e.g. `device.bus-id`, though this isn't super applicable to multi-node devices.
-                matches = [{ "node.name" = nodeName; "port.monitor" = "!true"; }];
               };
             in
             {
               "custom-device-names" = {
                 "monitor.alsa.rules" = [
-                  (setName "Trust GXT 232" "alsa_input.usb-145f_Trust_GXT_232_Microphone-00.mono-fallback")
-                  (setName "C270 Webcam" "alsa_input.usb-046d_C270_HD_WEBCAM_200901010001-02.mono-fallback")
+                  # External shared peripherals
+                  # NOTE: Try to keep matching rules tied to device rather than profiles
+                  (setName "Trust GXT 232" {
+                    "device.vendor.id" = "0x145f";
+                    "device.product.id" = "0x0254";
+                  })
+                  (setName "C270 Webcam" {
+                    "device.product.id" = "0x0825";
+                    "device.vendor.id" = "0x046d";
+                  })
 
-                  (setName "USB Hub Headphones Microphone" "alsa_input.usb-GeneralPlus_USB_Audio_Device-00.pro-input-0")
-                  (setName "USB Hub Headphones Microphone" "alsa_input.usb-GeneralPlus_USB_Audio_Device-00.mono-fallback")
-                  (setName "USB Hub Headphones" "alsa_output.usb-GeneralPlus_USB_Audio_Device-00.pro-output-0")
-                  (setName "USB Hub Headphones" "alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo")
-
-                  (setName "HDMI" "alsa_output.pci-0000_c2_00.1.hdmi-stereo")
+                  (setName "USB Hub Headphones Microphone" {
+                    "port.group" = "capture";
+                    "device.bus-id" = "usb-GeneralPlus_USB_Audio_Device-00";
+                  })
+                  (setName "USB Hub Headphones" {
+                    "port.group" = "playback";
+                    "device.bus-id" = "usb-GeneralPlus_USB_Audio_Device-00";
+                  })
 
                   # REVIEW Hardware-specific configurations - maybe move under system configs?
                   # masp
-                  (setName "On-board Microphone" "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source")
-                  (setName "On-board HDMI 1" "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI1__sink")
-                  (setName "On-board HDMI 2" "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI2__sink")
-                  (setName "On-board HDMI 3" "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI3__sink")
-                  (setName "On-board Headphones Microphone" "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic2__source")
-                  (setName "On-board Headphones" "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Headphones__sink")
+                  (setName "On-board Microphone" {
+                    "node.name" = "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source";
+                  })
+                  (setName "On-board HDMI 1" {
+                    "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI1__sink";
+                  })
+                  (setName "On-board HDMI 2" {
+                    "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI2__sink";
+                  })
+                  (setName "On-board HDMI 3" {
+                    "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI3__sink";
+                  })
+                  (setName "On-board Headphones Microphone" {
+                    "node.name" = "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic2__source";
+                  })
+                  (setName "On-board Headphones" {
+                    "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Headphones__sink";
+                  })
 
                   # go
-                  (setName "On-board Microphone" "alsa_input.pci-0000_c2_00.6.analog-stereo")
-                  (setName "On-board Speakers" "alsa_output.pci-0000_c2_00.6.analog-stereo")
+                  (setName "On-board HDMI" {
+                    "node.name" = "alsa_output.pci-0000_c2_00.1.hdmi-stereo";
+                  })
+                  (setName "On-board Microphone" {
+                    "node.name" = "alsa_input.pci-0000_c2_00.6.analog-stereo";
+                  })
+                  (setName "On-board Speakers" {
+                    "node.name" = "alsa_output.pci-0000_c2_00.6.analog-stereo";
+                  })
                 ];
               };
             };
