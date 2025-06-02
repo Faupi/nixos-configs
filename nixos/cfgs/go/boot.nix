df60859fa04ffdc29ab350cc818c81c0bd62cd12
Faupi <matej.sp583@gmail.com>
2025-06-03 01:41:12 +0200

    Go: Switched from systemd-boot to grub to resolve rollbacks

--- nixos/cfgs/go/boot.nix
+++ nixos/cfgs/go/boot.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   boot = {
     # NOTE: linux_zen 6.14.4 has sensor crash issues, trying xanmod for now
     kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_stable;
@@ -17,9 +17,16 @@
 
     loader = {
       timeout = 0; # Can't interact with it using the controller anyway. Dual-booting is preferable via the hardware boot menu
+      # Disable systemd-boot, as it holds onto old generations - TODO: Apply globally if it works
       systemd-boot = {
-        enable = true;
+        enable = lib.mkForce false;
         consoleMode = "max";
+      };
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        timeoutStyle = "countdown";
       };
     };
 
