9d6797629e70f16aa5f8f982e733e3743d462c57
Faupi <matej.sp583@gmail.com>
2025-06-26 00:19:20 +0200

    Go: Switched hhd to main user, fixed decky hhd user mapping

--- nixos/cfgs/go/handheld-daemon.nix
+++ nixos/cfgs/go/handheld-daemon.nix
@@ -1,19 +1,19 @@
 { lib, ... }: {
-  users.users.hhd = {
-    group = "hhd";
-    home = "/var/lib/handheld-daemon";
-    createHome = true;
-    isSystemUser = true;
-  };
-  users.groups.hhd = { };
+  # users.users.hhd = {
+  #   group = "hhd";
+  #   home = "/var/lib/handheld-daemon";
+  #   createHome = true;
+  #   isSystemUser = true;
+  # };
+  # users.groups.hhd = { };
 
   services = {
-    power-profiles-daemon.enable = lib.mkForce false; # Use PPD emulation in HHD
+    power-profiles-daemon.enable = true; # REVIEW: Seems to get disabled by handheld-daemon fine
     inputplumber.enable = lib.mkForce false; # REVIEW: steamos-manager might need it for something: https://github.com/Jovian-Experiments/Jovian-NixOS/commit/02a238236e3a859c43c7f940f2e86b281341a436
 
     handheld-daemon = {
       enable = true;
-      user = "hhd";
+      user = "faupi"; # NOTE: Should be main user!
       ui.enable = true;
       adjustor = {
         enable = true;
