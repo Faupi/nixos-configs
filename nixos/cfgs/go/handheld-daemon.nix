ef9e1f2109fc812577dca4a8ea4daa910a370e59
Faupi <matej.sp583@gmail.com>
2025-06-28 22:16:23 +0200

    Updated HHD

--- nixos/cfgs/go/handheld-daemon.nix
+++ nixos/cfgs/go/handheld-daemon.nix
@@ -1,14 +1,6 @@
 { lib, ... }: {
-  # users.users.hhd = {
-  #   group = "hhd";
-  #   home = "/var/lib/handheld-daemon";
-  #   createHome = true;
-  #   isSystemUser = true;
-  # };
-  # users.groups.hhd = { };
-
   services = {
-    power-profiles-daemon.enable = true; # REVIEW: Seems to get disabled by handheld-daemon fine
+    power-profiles-daemon.enable = lib.mkForce false;
     inputplumber.enable = lib.mkForce false; # REVIEW: steamos-manager might need it for something: https://github.com/Jovian-Experiments/Jovian-NixOS/commit/02a238236e3a859c43c7f940f2e86b281341a436
 
     handheld-daemon = {
