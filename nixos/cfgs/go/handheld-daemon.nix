cda995359a4277c756369b63b8e64166110b74ca
Faupi <matej.sp583@gmail.com>
2025-07-12 12:58:23 +0200

    go: locked HHD PPD setting

--- nixos/cfgs/go/handheld-daemon.nix
+++ nixos/cfgs/go/handheld-daemon.nix
@@ -1,6 +1,6 @@
 { lib, ... }: {
   services = {
-    power-profiles-daemon.enable = lib.mkForce false;
+    power-profiles-daemon.enable = true;
     inputplumber.enable = lib.mkForce false; # REVIEW: steamos-manager might need it for something: https://github.com/Jovian-Experiments/Jovian-NixOS/commit/02a238236e3a859c43c7f940f2e86b281341a436
 
     handheld-daemon = {
