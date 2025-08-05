7d68b34ccefbf4f3975e47dd1352ad7beb095fc5
Faupi <matej.sp583@gmail.com>
2025-08-05 22:31:22 +0200

    Go: Switched VR setup to wivrn+xrizer

--- nixos/cfgs/go/vr/default.nix
+++ nixos/cfgs/go/vr/default.nix
@@ -2,37 +2,13 @@
 {
   environment.systemPackages = with pkgs; [
     wlx-overlay-s # First time setup to run as `steam-run wlx-overlay-s`
+    xrizer
   ];
 
-  # SteamVR | NOTE: Needs to be configured manually in Steam `steamvr-wrapper %command%`
-  programs.steam.extraPackages = [
-    (pkgs.writeShellApplication {
-      name = "steamvr-wrapper";
-      runtimeInputs = with pkgs; [
-        jq
-        moreutils
-      ];
-      text = builtins.readFile ./steamvr-wrapper.sh;
-    })
-  ];
-
-  # ALVR
-  programs.alvr = {
+  services.wivrn = {
     enable = true;
+    package = pkgs.wivrn;
+    defaultRuntime = true;
     openFirewall = true;
-    package = pkgs.alvr;
   };
-
-  # Patch to resolve issues with async reprojection
-  # https://wiki.nixos.org/wiki/VR#SteamVR
-  # boot.kernelPatches = [
-  #   {
-  #     name = "amdgpu-ignore-ctx-privileges";
-  #     patch = pkgs.fetchpatch {
-  #       name = "cap_sys_nice_begone.patch";
-  #       url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-  #       hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-  #     };
-  #   }
-  # ];
 }
