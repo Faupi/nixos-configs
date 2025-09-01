f80642283ab51ffaba4e1b2beb8abd6e42a5553d
Faupi <matej.sp583@gmail.com>
2025-09-01 11:36:23 +0200

    Teams: Additional arguments

--- home-manager/cfgs/shared/teams/default.nix
+++ home-manager/cfgs/shared/teams/default.nix
@@ -15,6 +15,8 @@ let
         nameAffix = "xdg";
         arguments = [
           "--appIcon '${./icon/teams-light.png}'"
+          "--disableAutogain"
+          "--awayOnSystemIdle"
         ];
       }
     );
