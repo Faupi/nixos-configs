31a27a117f9734b1c850defecae0437a552379c8
Faupi <matej.sp583@gmail.com>
2025-10-04 01:09:25 +0200

    Lock revert and update for build-override

--- build-override.sh
+++ build-override.sh
@@ -1,1 +1,1 @@
-nix build --impure --builders "" --substituters "https://cache.nixos.org" --expr "(builtins.getFlake (toString $(dirname "$0")/.)).outputs.packages.x86_64-linux.$1.override {${2%;};}"
+nix build --impure --builders "" --substituters "https://cache.nixos.org" --expr "(builtins.getFlake (toString $(dirname "$0")/.)).outputs.legacyPackages.x86_64-linux.$1.override {${2%;};}"
