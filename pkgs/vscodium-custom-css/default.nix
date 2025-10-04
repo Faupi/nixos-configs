fdcfb6f33a83aa96e6886212dc23dc037528e7a6
Faupi <matej.sp583@gmail.com>
2025-10-04 02:47:42 +0200

    rewrite of the codium css handler to fhs bwrap because I love myself

--- pkgs/vscodium-custom-css/default.nix
+++ pkgs/vscodium-custom-css/default.nix
@@ -1,74 +1,50 @@
 { cssPath ? null
 
 , vscodium
-, symlinkJoin
 , runCommand
 , lib
 , nodejs
 , jq
 , moreutils
-, bubblewrap
+, buildFHSEnvBubblewrap
 }:
-assert lib.assertMsg (builtins.typeOf cssPath == "path") "vscodium-custom-css: cssPath must be a path literal";
-assert lib.assertMsg (builtins.pathExists cssPath) "vscodium-custom-css: cssPath does not exist";
+# assert lib.assertMsg (builtins.typeOf cssPath == "path") "vscodium-custom-css: cssPath must be a path literal";
+# assert lib.assertMsg (builtins.pathExists cssPath) "vscodium-custom-css: cssPath does not exist";
 let
-  # TODO: Rewrite to FHS handling
-  vscodium-custom-css =
+  resDir = "lib/vscode/resources";
+  appOutDir = "${resDir}/app/out";
+  appVSRel = "vs/code";
+  productPath = "${resDir}/app/product.json";
+
+  wbPathsInternal = [
+    "${appVSRel}/electron-sandbox/workbench/workbench.html"
+    "${appVSRel}/electron-browser/workbench/workbench.html"
+  ];
+  wbPathInternal = lib.findFirst (p: builtins.pathExists "${vscodium.out}/${appOutDir}/${p}") (throw "Workbench.html could not be found") wbPathsInternal;
+  wbPath = "${appOutDir}/${wbPathInternal}";
+
+  overlay =
     runCommand "vscodium-custom-css-bit" { } ''
-      orig="${vscodium}"
-      res="lib/vscode/resources"
-      appDir="$res/app/out/vs/code"
-
-      wbRelPath=""
-      for candidate in \
-        "electron-sandbox/workbench" \
-        "electron-browser/workbench"
-      do
-        if [ -e "$orig/$appDir/$candidate/workbench.html" ]; then
-          wbRelPath="$appDir/$candidate/workbench.html"
-          break
-        fi
-      done
-
-      if [ -z "$wbRelPath" ]; then
-        echo "Could not locate workbench.html" >&2
-        exit 1
-      fi
+      orig="${vscodium.out}"
 
       echo "Add custom CSS"
-      install -D "$orig/$wbRelPath" "$out/$wbRelPath"
-      substituteInPlace "$out/$wbRelPath" \
+      install -D "$orig/${wbPath}" "$out/${wbPath}"
+      substituteInPlace "$out/${wbPath}" \
         --replace-fail '<head>' '<head><style type="text/css">${builtins.replaceStrings [ "'" ] [ "'\\''" ] (builtins.readFile cssPath)}</style>'
 
       echo "Update checksum of HTML with custom CSS"
-      checksum=$(${lib.getExe nodejs} ${./print-checksum.js} "$out/$wbRelPath")
-      productPath="lib/vscode/resources/app/product.json"
-      ${lib.getExe jq} ".checksums.\"$wbRelPath\" = \"$checksum\"" "$orig/$productPath" | ${lib.getExe' moreutils "sponge"} "$out/$productPath"
-
-      # ==Overlay the binary==
-      mkdir -p $out/bin
-
-      cat > $out/bin/codium <<SH
-      #!/usr/bin/env bash
-      set -euo pipefail
-
-      # Compose a minimal bwrap sandbox:
-      # - Bind the whole root as-is
-      # - Bind *overlayed* files on top of the base store paths
-      exec ${lib.getExe bubblewrap} \
-        --unshare-user-try \
-        --bind / / \
-        --dev-bind /dev /dev \
-        --proc /proc \
-        --ro-bind "$out/$wbRelPath" "$orig/$wbRelPath" \
-        --ro-bind "$out/$productPath" "$orig/$productPath" \
-        "$orig/bin/codium" --no-cached-data "\$@"
-      SH
-      chmod +x $out/bin/codium
+      checksum=$(${lib.getExe nodejs} ${./print-checksum.js} "$out/${wbPath}")
+      ${lib.getExe jq} ".checksums.\"${wbPathInternal}\" = \"$checksum\"" "$orig/${productPath}" | ${lib.getExe' moreutils "sponge"} "$out/${productPath}"
     '';
 in
-symlinkJoin {
-  name = "vscodium-custom-css";
-  inherit (vscodium) pname version meta;
-  paths = [ vscodium-custom-css vscodium ];
+buildFHSEnvBubblewrap {
+  name = "codium";
+  targetPkgs = p: [ ];
+  runScript = "${lib.getExe vscodium}";
+
+  extraBwrapArgs =
+    [
+      "--ro-bind ${overlay.out}/${wbPath} ${vscodium.out}/${wbPath}"
+      "--ro-bind ${overlay.out}/${productPath} ${vscodium.out}/${productPath}"
+    ];
 }
