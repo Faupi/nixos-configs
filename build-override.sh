692f0e14e37991b448feaec729c9c6ebff95bcac
Faupi <matej.sp583@gmail.com>
2025-10-04 02:47:22 +0200

    unfucked build override args

--- build-override.sh
+++ build-override.sh
@@ -1,1 +1,5 @@
-nix build --impure --builders "" --substituters "https://cache.nixos.org" --expr "(builtins.getFlake (toString $(dirname "$0")/.)).outputs.legacyPackages.x86_64-linux.$1.override {${2%;};}"
+#!/bin/bash
+pkgName=$1
+opts=$2
+shift 2
+nix build --impure --builders "" --substituters "https://cache.nixos.org" --expr "(builtins.getFlake (toString $(dirname "$0")/.)).outputs.legacyPackages.x86_64-linux.$pkgName.override {${opts%;};}" "$@"
