5eb61938c1d808fa059809273e51c65b315c360f
Faupi <matej.sp583@gmail.com>
2025-10-07 00:45:04 +0200

    Plasma: Shared top launcher panel

--- home-manager/cfgs/shared/kde-plasma/panels/default.nix
+++ home-manager/cfgs/shared/kde-plasma/panels/default.nix
@@ -17,18 +17,19 @@ in
       kde.plasmoid-button
     ];
 
-    programs.plasma.panels = [
-      {
-        location = "top";
-        floating = false;
-        hiding = "none";
-        alignment = "center";
-        opacity = "opaque";
-        screen = 0;
-        height = 28;
-        lengthMode = "fill";
-        widgets = [
-          /* TODO: Unearth for touch mode
+    programs.plasma.panels =
+      let
+        topBar = {
+          location = "top";
+          floating = false;
+          hiding = "none";
+          alignment = "center";
+          opacity = "opaque";
+          screen = 0;
+          height = 28;
+          lengthMode = "fill";
+          widgets = [
+            /* TODO: Unearth for touch mode
           {
             name = "p-connor.plasma-drawer"; # req: pkgs.plasma-drawer
             config = {
@@ -55,213 +56,214 @@ in
               };
             };
           }
-          */
+            */
 
-          {
-            kickoff = {
-              icon = cfg.launcherIcon;
-              sidebarPosition = "left";
-              showActionButtonCaptions = false;
-              showButtonsFor.custom = [
-                "suspend"
-                "lock-screen"
-                "logout"
-              ];
-            };
-          }
-
-          {
-            name = "org.dhruv8sh.kara";
-            config = {
-              # NOTE: lowercase "general"
-              general = {
-                type = 0; # Pills
-                animationDuration = 200; # ms
-              };
-              type1 = {
-                t1activeWidth = 20;
-                t1activeHeight = 10;
-                t1height = 10;
-                t1width = 10;
-              };
-            };
-          }
-
-          "org.kde.plasma.panelspacer"
-
-          {
-            systemMonitor = {
-              displayStyle = "org.kde.ksysguard.horizontalbars";
-              title = "System Resources - Processing";
-              showTitle = true;
-              showLegend = true;
-              settings = {
-                "org.kde.ksysguard.horizontalbars/General" = {
-                  rangeAuto = false;
-                  rangeFrom = 0;
-                  rangeTo = 100;
-                };
-              };
-              sensors = [
-                {
-                  name = "cpu/all/usage";
-                  color = sharedOptions.colorCPU;
-                  label = "CPU";
-                }
-                {
-                  name = "gpu/all/usage";
-                  color = sharedOptions.colorGPU;
-                  label = "GPU";
-                }
-              ];
-            };
-          }
-
-          {
-            digitalClock = {
-              time = {
-                showSeconds = "onlyInTooltip";
-                format = "24h";
-              };
-              date = {
-                enable = true;
-                format = { custom = "dd/MM/yyyy"; };
-                position = "besideTime";
-              };
-              calendar = {
-                firstDayOfWeek = "monday";
-                # TODO: Add calendar plugins?
-              };
-            };
-          }
-
-          {
-            systemMonitor = {
-              displayStyle = "org.kde.ksysguard.horizontalbars";
-              title = "System Resources - Memory";
-              showTitle = true;
-              showLegend = true;
-              settings = {
-                "org.kde.ksysguard.horizontalbars/General" = {
-                  rangeAuto = false;
-                  rangeFrom = 0;
-                  rangeTo = 100;
-                };
-              };
-              sensors = [
-                {
-                  name = "memory/physical/usedPercent";
-                  color = sharedOptions.colorMemory;
-                  label = "Memory";
-                }
-                {
-                  name = "memory/swap/usedPercent";
-                  color = sharedOptions.colorSwap;
-                  label = "Swap";
-                }
-              ];
-            };
-          }
-
-          "org.kde.plasma.panelspacer"
-
-          (mkIf hasMonitorSwitcher {
-            name = "com.github.configurable_button";
-            config = {
-              onScriptEnabled = true;
-              onScript = "systemctl start --user monitor-input-switcher";
-              iconOn = toString ./monitor-switcher-enabled.svg;
-
-              offScriptEnabled = true;
-              offScript = "systemctl stop --user monitor-input-switcher";
-              iconOff = toString ./monitor-switcher-disabled.svg;
-
-              statusScriptEnabled = true;
-              statusScript = "systemctl status --user monitor-input-switcher | grep 'Active: active'";
-              runStatusOnStart = true;
-              interval = 300; # in seconds
-              updateInterval = 5;
-              updateIntervalUnit = 1; # 0 s, 1 m, 2 h
-            };
-          })
-
-          {
-            systemTray = {
-              icons = {
-                spacing = "small";
-                scaleToFit = false;
-              };
-              items = {
-                showAll = false;
-                shown = [
-                  "org.kde.plasma.battery"
-                  "org.kde.plasma.volume"
-                  "org.kde.plasma.keyboardlayout"
-                ];
-                hidden = [
-                  "org.kde.kalendar.contact"
-                  "org.kde.plasma.clipboard"
-                  "org.kde.kscreen"
-                  "org.kde.plasma.devicenotifier"
-                  "org.kde.plasma.brightness"
-                  "org.kde.kdeconnect"
-                  "Discover Notifier_org.kde.DiscoverNotifier"
-                  "Wallet Manager"
-                  "KDE Daemon"
-                  "The KDE Crash Handler"
-                  "touchpad"
-                  "spotify-client"
+            {
+              kickoff = {
+                icon = cfg.launcherIcon;
+                sidebarPosition = "left";
+                showActionButtonCaptions = false;
+                showButtonsFor.custom = [
+                  "suspend"
+                  "lock-screen"
+                  "logout"
                 ];
               };
-            };
-          }
-        ];
-      }
+            }
 
-      {
-        location = "bottom";
-        floating = true;
-        hiding = "dodgewindows";
-        alignment = "center";
-        opacity = "translucent";
-        screen = 0;
-        height = 56;
-        lengthMode = "fit";
-        widgets = [
-          {
-            iconTasks = {
-              launchers = [
-                "preferred://filemanager"
-                "preferred://browser"
-              ];
-              appearance = {
-                showTooltips = true;
-                highlightWindows = true;
-                indicateAudioStreams = true;
-                fill = true;
-                rows = {
-                  multirowView = "never";
+            {
+              name = "org.dhruv8sh.kara";
+              config = {
+                # NOTE: lowercase "general"
+                general = {
+                  type = 0; # Pills
+                  animationDuration = 200; # ms
                 };
-                iconSpacing = "medium";
+                type1 = {
+                  t1activeWidth = 20;
+                  t1activeHeight = 10;
+                  t1height = 10;
+                  t1width = 10;
+                };
               };
-              behavior = {
-                grouping = {
-                  method = "none";
+            }
+
+            "org.kde.plasma.panelspacer"
+
+            {
+              systemMonitor = {
+                displayStyle = "org.kde.ksysguard.horizontalbars";
+                title = "System Resources - Processing";
+                showTitle = true;
+                showLegend = true;
+                settings = {
+                  "org.kde.ksysguard.horizontalbars/General" = {
+                    rangeAuto = false;
+                    rangeFrom = 0;
+                    rangeTo = 100;
+                  };
                 };
-                middleClickAction = "newInstance";
-                showTasks = {
-                  onlyInCurrentScreen = false;
-                  onlyInCurrentDesktop = true;
-                  onlyInCurrentActivity = true;
-                  onlyMinimized = false;
-                };
-                unhideOnAttentionNeeded = true;
-                newTasksAppearOn = "right";
+                sensors = [
+                  {
+                    name = "cpu/all/usage";
+                    color = sharedOptions.colorCPU;
+                    label = "CPU";
+                  }
+                  {
+                    name = "gpu/all/usage";
+                    color = sharedOptions.colorGPU;
+                    label = "GPU";
+                  }
+                ];
               };
-            };
-          }
-        ];
-      }
-    ];
+            }
+
+            {
+              digitalClock = {
+                time = {
+                  showSeconds = "onlyInTooltip";
+                  format = "24h";
+                };
+                date = {
+                  enable = true;
+                  format = { custom = "dd/MM/yyyy"; };
+                  position = "besideTime";
+                };
+                calendar = {
+                  firstDayOfWeek = "monday";
+                  # TODO: Add calendar plugins?
+                };
+              };
+            }
+
+            {
+              systemMonitor = {
+                displayStyle = "org.kde.ksysguard.horizontalbars";
+                title = "System Resources - Memory";
+                showTitle = true;
+                showLegend = true;
+                settings = {
+                  "org.kde.ksysguard.horizontalbars/General" = {
+                    rangeAuto = false;
+                    rangeFrom = 0;
+                    rangeTo = 100;
+                  };
+                };
+                sensors = [
+                  {
+                    name = "memory/physical/usedPercent";
+                    color = sharedOptions.colorMemory;
+                    label = "Memory";
+                  }
+                  {
+                    name = "memory/swap/usedPercent";
+                    color = sharedOptions.colorSwap;
+                    label = "Swap";
+                  }
+                ];
+              };
+            }
+
+            "org.kde.plasma.panelspacer"
+
+            (mkIf hasMonitorSwitcher {
+              name = "com.github.configurable_button";
+              config = {
+                onScriptEnabled = true;
+                onScript = "systemctl start --user monitor-input-switcher";
+                iconOn = toString ./monitor-switcher-enabled.svg;
+
+                offScriptEnabled = true;
+                offScript = "systemctl stop --user monitor-input-switcher";
+                iconOff = toString ./monitor-switcher-disabled.svg;
+
+                statusScriptEnabled = true;
+                statusScript = "systemctl status --user monitor-input-switcher | grep 'Active: active'";
+                runStatusOnStart = true;
+                interval = 300; # in seconds
+                updateInterval = 5;
+                updateIntervalUnit = 1; # 0 s, 1 m, 2 h
+              };
+            })
+
+            {
+              systemTray = {
+                icons = {
+                  spacing = "small";
+                  scaleToFit = false;
+                };
+                items = {
+                  showAll = false;
+                  shown = [
+                    "org.kde.plasma.battery"
+                    "org.kde.plasma.volume"
+                    "org.kde.plasma.keyboardlayout"
+                  ];
+                  hidden = [
+                    "org.kde.kalendar.contact"
+                    "org.kde.plasma.clipboard"
+                    "org.kde.kscreen"
+                    "org.kde.plasma.devicenotifier"
+                    "org.kde.plasma.brightness"
+                    "org.kde.kdeconnect"
+                    "Discover Notifier_org.kde.DiscoverNotifier"
+                    "Wallet Manager"
+                    "KDE Daemon"
+                    "The KDE Crash Handler"
+                    "touchpad"
+                    "spotify-client"
+                  ];
+                };
+              };
+            }
+          ];
+        };
+
+        launcherPanel = {
+          location = "top";
+          floating = true;
+          hiding = "dodgewindows";
+          alignment = "center";
+          opacity = "translucent";
+          screen = 0;
+          height = 56;
+          lengthMode = "fit";
+          widgets = [
+            {
+              iconTasks = {
+                launchers = [
+                  "preferred://filemanager"
+                  "preferred://browser"
+                ];
+                appearance = {
+                  showTooltips = true;
+                  highlightWindows = true;
+                  indicateAudioStreams = true;
+                  fill = true;
+                  rows = {
+                    multirowView = "never";
+                  };
+                  iconSpacing = "medium";
+                };
+                behavior = {
+                  grouping = {
+                    method = "none";
+                  };
+                  middleClickAction = "newInstance";
+                  showTasks = {
+                    onlyInCurrentScreen = false;
+                    onlyInCurrentDesktop = true;
+                    onlyInCurrentActivity = true;
+                    onlyMinimized = false;
+                  };
+                  unhideOnAttentionNeeded = true;
+                  newTasksAppearOn = "right";
+                };
+              };
+            }
+          ];
+        };
+      in
+      [ topBar ] ++ (flip genList 2 (i: launcherPanel // { screen = i; }));
   };
 }
