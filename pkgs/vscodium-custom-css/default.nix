42656fd2165df48ad37a141c963448d06973ad26
Faupi <matej.sp583@gmail.com>
2025-11-10 00:06:20 +0100

    VSCodium-CSS: Updated definition

--- pkgs/vscodium-custom-css/default.nix
+++ pkgs/vscodium-custom-css/default.nix
@@ -8,7 +8,7 @@
 , moreutils
 , nodejs
 , runCommand
-, writeShellScriptBin
+, writeShellApplication
 , symlinkJoin
 , vscodium
 , systemd
@@ -35,35 +35,43 @@ let
     then builtins.replaceStrings [ "'" ] [ "'\\''" ] (builtins.readFile cssPath)
     else throw "Invalid CSS path supplied";
 
-  sponge = lib.getExe' moreutils "sponge";
-  patched = runCommand "vscodium-custom-css-patched" { } /*sh*/''
+  patched = runCommand "vscodium-custom-css-patched"
+    {
+      buildInputs = [ jq moreutils nodejs ];
+      inherit wbRel wbRelFull productRel cssContent;
+      orig = vscodium.out;
+      checksumGetter = ./print-checksum.js;
+    } /*sh*/''
     set -euo pipefail
-    orig="${vscodium.out}"
 
     # copy and patch workbench.html
-    install -D "$orig/${wbRelFull}" "$out/${wbRelFull}"
-    substituteInPlace "$out/${wbRelFull}" \
-      --replace-fail '<head>' '<head><style type="text/css">${cssContent}</style>'
+    install -D "$orig/$wbRelFull" "$out/$wbRelFull"
+    substituteInPlace "$out/$wbRelFull" \
+      --replace-fail '<head>' "<head><style type=\"text/css\">$cssContent</style>"
 
     # update product.json checksum
-    install -D "$orig/${productRel}" "$out/${productRel}"
-    checksum=$(${lib.getExe nodejs} ${./print-checksum.js} "$out/${wbRelFull}")
-    ${lib.getExe jq} ".checksums.\"${wbRel}\" = \"$checksum\"" \
-      "$out/${productRel}" | ${sponge} "$out/${productRel}"
+    install -D "$orig/$productRel" "$out/$productRel"
+    checksum=$(node $checksumGetter "$out/$wbRelFull")
+    jq ".checksums.\"$wbRel\" = \"$checksum\"" "$out/$productRel" | sponge "$out/$productRel"
   '';
 
-  wrapper = writeShellScriptBin "codium" /*sh*/''
-    set -euo pipefail
+  wrapper = writeShellApplication {
+    name = "codium";
+    runtimeInputs = [ bubblewrap ];
+    runtimeEnv = { inherit patched systemd vscodium wbRelFull productRel; };
+    text = /*sh*/''
+      set -euo pipefail
 
-    exec ${lib.getExe bubblewrap} \
-      --bind / / \
-      --proc /proc \
-      --dev-bind /dev /dev \
-      --tmpfs ${systemd}/lib/systemd/ssh_config.d \
-      --bind ${patched}/${wbRelFull} ${vscodium.out}/${wbRelFull} \
-      --bind ${patched}/${productRel} ${vscodium.out}/${productRel} \
-      ${vscodium.out}/bin/codium --no-sandbox "$@"
-  '';
+      exec bwrap \
+        --bind / / \
+        --proc /proc \
+        --dev-bind /dev /dev \
+        --tmpfs $systemd/lib/systemd/ssh_config.d \
+        --bind $patched/$wbRelFull $vscodium/$wbRelFull \
+        --bind $patched/$productRel $vscodium/$productRel \
+        $vscodium/bin/codium --no-sandbox "$@"
+    '';
+  };
 in
 symlinkJoin {
   name = "vscodium-custom-css";
