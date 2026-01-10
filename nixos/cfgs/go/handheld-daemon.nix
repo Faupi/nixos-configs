48057a070e66ddf9a66ddc100131872624400fed
Faupi <matej.sp583@gmail.com>
2026-01-10 21:20:13 +0100

    HHD: Fix missing adjustor module

--- nixos/cfgs/go/handheld-daemon.nix
+++ nixos/cfgs/go/handheld-daemon.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, ... }:
+let
+  # TODO: Remove wrapper when updating from 25.11 NixOS / to 4+ HHD
+  adjustorPyPath = pkgs.python3Packages.makePythonPath [ pkgs.adjustor ];
+in
+{
   services = {
     power-profiles-daemon.enable = false; # Still can't decide whether to use PPD or HHD c:
     inputplumber.enable = lib.mkForce false; # REVIEW: steamos-manager might need it for something: https://github.com/Jovian-Experiments/Jovian-NixOS/commit/02a238236e3a859c43c7f940f2e86b281341a436
@@ -6,16 +11,16 @@
     handheld-daemon = {
       enable = true;
       user = "faupi"; # NOTE: Should be main user!
-      package = pkgs.unstable.handheld-daemon;
-      ui = {
-        enable = true;
-        package = pkgs.unstable.handheld-daemon-ui;
-      };
+      ui.enable = true;
       adjustor = {
         enable = true;
-        package = pkgs.unstable.adjustor;
         loadAcpiCallModule = true;
       };
     };
+  };
+
+  systemd.services.handheld-daemon.environment = {
+    PYTHONPATH = adjustorPyPath;
+    PYTHONNOUSERSITE = "1"; # Don't pull user packages
   };
 }
