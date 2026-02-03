5511c50f55f3f16fe1f9fef9397fa4ef0d2e82f0
Faupi <matej.sp583@gmail.com>
2026-02-03 19:41:50 +0100

    Powerdevil: Expand base config

--- home-manager/cfgs/shared/kde-plasma/powerdevil.nix
+++ home-manager/cfgs/shared/kde-plasma/powerdevil.nix
@@ -1,20 +1,64 @@
-{ lib, cfg, ... }:
+{ lib, cfg, fop-utils, ... }:
+let
+  minutes = sec: sec * 60;
+in
 {
   config = lib.mkIf cfg.enable {
     programs.plasma = {
-      powerdevil = {
+      powerdevil = fop-utils.mkDefaultRecursively {
         general = {
           pausePlayersOnSuspend = false;
         };
+
+        batteryLevels = {
+          lowLevel = 10;
+          criticalLevel = 5;
+          criticalAction = "hibernate"; # Should be fine even for systems without hibernation
+        };
+
         AC = {
+          powerProfile = "performance";
           powerButtonAction = "sleep";
+          autoSuspend = {
+            idleTimeout = minutes 15;
+            action = "sleep";
+          };
+          displayBrightness = null;
+          dimDisplay = {
+            enable = true;
+            idleTimeout = minutes 5;
+          };
+          turnOffDisplay.idleTimeout = "never";
         };
+
         battery = {
+          powerProfile = "balanced";
           powerButtonAction = "sleep";
+          autoSuspend = {
+            idleTimeout = minutes 5;
+            action = "sleep";
+          };
+          displayBrightness = null;
+          dimDisplay = {
+            enable = true;
+            idleTimeout = minutes 2;
+          };
+          turnOffDisplay.idleTimeout = "never";
         };
+
         lowBattery = {
-          powerButtonAction = "sleep";
           powerProfile = "powerSaving";
+          powerButtonAction = "sleep";
+          autoSuspend = {
+            idleTimeout = minutes 2;
+            action = "sleep";
+          };
+          displayBrightness = 30;
+          dimDisplay = {
+            enable = true;
+            idleTimeout = minutes 1;
+          };
+          turnOffDisplay.idleTimeout = "never";
         };
       };
 
@@ -25,6 +69,10 @@
               "Steam:Client interface" # Steam does this at random in the tray
             ];
           };
+
+          # TODO: Send PR for upstream fix (null sets null option instead of false)
+          "AC/Display".UseProfileSpecificDisplayBrightness = false;
+          "Battery/Display".UseProfileSpecificDisplayBrightness = false;
         };
       };
     };
