4715911d3e826b394cebd474d0538a47f363383b
Faupi <matej.sp583@gmail.com>
2026-02-24 11:03:55 +0100

    feat(go): enable sysctl config from steamos

--- nixos/cfgs/go/steam/steamos.nix
+++ nixos/cfgs/go/steam/steamos.nix
@@ -3,5 +3,6 @@
     useSteamOSConfig = lib.mkForce false; # No automatic enabling of stuff in the steamos module
     enableProductSerialAccess = true;
     enableAutoMountUdevRules = true;
+    enableSysctlConfig = true; # Latency optimizations from SteamOS
   };
 }
