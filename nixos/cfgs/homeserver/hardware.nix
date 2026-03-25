ec8710d2b26491e53998bbbcd54762631cd81ace
Faupi <matej.sp583@gmail.com>
2026-03-25 22:23:09 +0100

    gamestream: enable microcode explicitly

--- nixos/cfgs/homeserver/hardware.nix
+++ nixos/cfgs/homeserver/hardware.nix
@@ -19,5 +19,7 @@
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.intel.updateMicrocode = true;
 }
