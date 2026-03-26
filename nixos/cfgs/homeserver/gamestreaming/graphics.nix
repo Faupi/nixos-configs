eb570659b81dcf63e199136a5cc380dab3fc7407
Faupi <matej.sp583@gmail.com>
2026-03-26 23:02:41 +0100

    gamestream: forego swapping mesa as it causes more harm than good

--- nixos/cfgs/homeserver/gamestreaming/graphics.nix
+++ nixos/cfgs/homeserver/gamestreaming/graphics.nix
@@ -3,10 +3,6 @@
     sessionVariables = {
       PROTON_FSR4_UPGRADE = 1; # FSR 3.1+ gets upgraded to FSR4 
       ENABLE_LAYER_MESA_ANTI_LAG = 1; # Improves latency (mesa 25.3+)
-
-      # Remove these if the system is alright without them
-      LIBVA_DRIVER_NAME = "radeonsi";
-      LIBVA_DRIVERS_PATH = "/run/opengl-driver/lib/dri";
     };
     systemPackages = with pkgs; [
       amdgpu_top
@@ -34,9 +30,6 @@
     graphics = {
       enable = true;
       enable32Bit = true;
-      # Make sure to keep graphics drivers fully up to date. Good features yo.
-      package = pkgs.mesa-25_3_5;
-      package32 = pkgs.mesa32-25_3_5;
     };
   };
 }
