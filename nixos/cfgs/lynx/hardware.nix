f4f5ed245718b9b33d02316deb0439c6720d9ff8
Faupi <matej.sp583@gmail.com>
2026-04-03 19:52:56 +0200

    lynx: remove kvm-amd and cpu freq governor in hopes the clocks will function better

--- nixos/cfgs/lynx/hardware.nix
+++ nixos/cfgs/lynx/hardware.nix
@@ -1,15 +1,8 @@
 { ... }:
 {
-  boot = {
-    kernelModules = [
-      "kvm-amd"
-    ];
-    kernelParams = [
-      "amd_pstate=active" # Should stabilize clocks (was boosting to max 4.2GHz)
-    ];
-  };
-
-  powerManagement.cpuFreqGovernor = "performance";
+  boot.kernelParams = [
+    "amd_pstate=active" # Should stabilize clocks (was boosting to max 4.2GHz)
+  ];
 
   hardware = {
     enableRedistributableFirmware = true;
