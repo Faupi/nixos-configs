5306bdd2fd0448bcf9da831af0362631aa6f02fe
Faupi <matej.sp583@gmail.com>
2026-04-05 22:21:06 +0200

    vr: force wayvr path more directly

--- nixos/cfgs/shared/vr/default.nix
+++ nixos/cfgs/shared/vr/default.nix
@@ -34,9 +34,13 @@ in
       config = {
         enable = true;
         json = {
-          application = [
-            wayvr # NOTE: Needs to be the package directly
-          ];
+          # TODO: Maybe use an auto-setup script https://github.com/Kirottu/nixos/blob/8de3a5503fa31cd73a545a15e1a2f33a8ecc9735/modules/gaming/vr/default.nix#L214-L294
+          application = (pkgs.writeShellApplication {
+            name = "wivrn-autostart";
+            text = ''
+              ${lib.getExe wayvr} --openxr --show
+            '';
+          });
           openvr-compat-path = xrizerlib;
           bitrate = 100 * 1000 * 1000; #Mbit
           debug-gui = false;
