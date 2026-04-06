8dab4ba227aa92d867e4cd7ce8731bc50be2f9c9
Faupi <matej.sp583@gmail.com>
2026-04-06 14:16:32 +0200

    moonlight: fix mic wrapper

--- home-manager/cfgs/faupi/graphical/moonlight.nix
+++ home-manager/cfgs/faupi/graphical/moonlight.nix
@@ -1,42 +1,70 @@
 { pkgs, lib, ... }:
 let
-  moonlightStreamCommand = "${lib.getExe pkgs.moonlight-qt} stream lynx 'Desktop (Mic)' --quit-after";
+  package = pkgs.moonlight-qt;
 
-  moonlightDesktopTemplate = {
-    name = "com.moonlight_stream.Moonlight-Lynx";
-    comment = "Stream games from your NVIDIA GameStream-enabled PC";
-    desktopName = "Moonlight (Lynx)";
-    exec = moonlightStreamCommand;
-    terminal = false;
-    icon = "moonlight";
-    type = "Application";
-    categories = [ "Qt" "Game" ];
-    keywords = [ "nvidia" "gamestream" "stream" ];
+  moonlight-lynx-mic = rec {
+    cmd = pkgs.writeShellApplication {
+      name = "moonlight-lynx-mic";
+      runtimeInputs = with pkgs; [
+        package
+        pipewire
+      ];
+      text = /*sh*/''
+        # Kill backgrounded ROC and Loopback processes on exit
+        trap 'kill $(jobs -p)' EXIT SIGINT SIGTERM
+
+        # Network pipe
+        pw-cli -m load-module libpipewire-module-roc-sink '{
+          fec.code = "rs8m",
+          remote.ip = "192.168.0.100",
+          remote.source.port = 10001,
+          remote.repair.port = 10002,
+          remote.control.port = 10003,
+          sink.props = {
+            node.name = "moonlight-mic-sender",
+            media.class = "Audio/Sink"
+          }
+        }' &
+
+        # Give PipeWire a heartbeat to register the sink
+        sleep 0.2
+
+        # Connect default mic to pipe
+        pw-cli -m load-module libpipewire-module-loopback '{
+          capture.props = {
+            node.target = "@DEFAULT_SOURCE@",
+            media.class = "Stream/Input/Audio",
+            node.description = "Moonlight Mic Bridge"
+          },
+          playback.props = {
+            node.target = "moonlight-mic-sender",
+            media.class = "Stream/Output/Audio"
+          }
+        }' &
+
+        ${lib.getExe package} stream lynx 'Desktop (Mic)' --quit-after
+      '';
+    };
+
+    desktopfile = pkgs.makeDesktopItem {
+      name = "com.moonlight_stream.Moonlight-Lynx_MIC";
+      desktopName = "Moonlight (Lynx) - MIC";
+      comment = "Stream games from your NVIDIA GameStream-enabled PC";
+      terminal = false;
+      icon = "moonlight";
+      type = "Application";
+      categories = [ "Qt" "Game" ];
+      keywords = [ "nvidia" "gamestream" "stream" ];
+      exec = lib.getExe cmd;
+    };
   };
-
-  moonlight-mic-wrapper = pkgs.makeDesktopItem (moonlightDesktopTemplate // {
-    name = "${moonlightDesktopTemplate.name}_MIC";
-    exec = toString (pkgs.writeShellScript "moonlight-mic-wrapper" /*sh*/''
-      trap 'kill %1' SIGINT
-      pw-cli -m load-module libpipewire-module-roc-sink \
-        fec.code=rs8m \
-        remote.ip=192.168.0.100 \
-        remote.source.port=10001 \
-        remote.repair.port=10002 \
-        remote.control.port=10003 \
-        sink.name="ROC Network Sink" \
-        sink.props="{ \
-          node.name=\"moonlight-mic-sender\" \
-          media.class=\"Audio/Sink\" \
-        }" &
-      ${moonlightStreamCommand}
-    '');
-    desktopName = "${moonlightDesktopTemplate.desktopName} - MIC";
-  });
 in
 {
   home.packages = with pkgs; [
     moonlight-qt
-    moonlight-mic-wrapper
-  ];
+  ]
+  ++ (with moonlight-lynx-mic; [
+    cmd
+    desktopfile
+  ]);
 }
