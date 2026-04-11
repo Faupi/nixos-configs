bf13d73e922757d127c7c266025659b154c7584e
Faupi <matej.sp583@gmail.com>
2026-04-11 14:56:26 +0200

    lynx: fix sleep syntax

--- nixos/cfgs/lynx/sleep.nix
+++ nixos/cfgs/lynx/sleep.nix
@@ -4,40 +4,42 @@ let
   minutes /*to seconds*/ = min: min * 60;
 in
 {
-  # Stop logind from potentially messing with autosuspend
-  logind.settings.Login.IdleAction = "ignore";
+  services = {
+    # Stop logind from potentially messing with autosuspend
+    logind.settings.Login.IdleAction = "ignore";
 
-  autosuspend = {
-    enable = true;
+    autosuspend = {
+      enable = true;
 
-    settings = {
-      idle_time = minutes 1;
-      interval = minutes 1;
-    };
-
-    checks = {
-      # Check for systemd inhibitors
-      Inhibitors = {
-        enable = true;
-        class = "ExternalCommand";
-        command = getExe (pkgs.writeShellApplication {
-          name = "autosuspend-systemd-inhibit";
-          runtimeInputs = with pkgs; [
-            systemd
-            gnugrep
-          ];
-          text = /*sh*/''
-            systemd-inhibit --list --mode=block --no-legend |
-              grep -q .
-          '';
-        });
+      settings = {
+        idle_time = minutes 1;
+        interval = minutes 1;
       };
 
-      # Don't sleep when SSH sessions are active
-      RemoteUsers = {
-        enable = true;
-        class = "Users";
-        host = ".+"; # Non-empty = remote
+      checks = {
+        # Check for systemd inhibitors
+        Inhibitors = {
+          enable = true;
+          class = "ExternalCommand";
+          command = getExe (pkgs.writeShellApplication {
+            name = "autosuspend-systemd-inhibit";
+            runtimeInputs = with pkgs; [
+              systemd
+              gnugrep
+            ];
+            text = /*sh*/''
+              systemd-inhibit --list --mode=block --no-legend |
+                grep -q .
+            '';
+          });
+        };
+
+        # Don't sleep when SSH sessions are active
+        RemoteUsers = {
+          enable = true;
+          class = "Users";
+          host = ".+"; # Non-empty = remote
+        };
       };
     };
   };
