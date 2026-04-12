5237eab790772a53214dc3fdb0b69c54f8a4a15f
Faupi <matej.sp583@gmail.com>
2026-04-12 20:28:20 +0200

    lynx: enable extra flags to try to match legion

--- nixos/cfgs/lynx/boot.nix
+++ nixos/cfgs/lynx/boot.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }: {
   boot = {
     kernelPackages = pkgs.linuxKernel.packagesFor pkgs.cachyosKernels.linux-cachyos-latest-lto-x86_64-v3;
+    kernelParams = [
+      # FIXME: Temporary flags to match Legion
+      "amdgpu.sg_display=0"
+      "amdgpu.dcdebugmask=0x10"
+
+      "split_lock_detect=off"
+      "preempt=full"
+    ];
     initrd.availableKernelModules = [
       "nvme"
       "xhci_pci"
