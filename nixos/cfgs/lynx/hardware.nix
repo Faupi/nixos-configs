e1ffb8f20cbfb539da6d69adad5a5f0b3cd573b0
Faupi <matej.sp583@gmail.com>
2026-05-19 12:33:01 +0200

    lynx: add zenpower for cpu power reads

--- nixos/cfgs/lynx/hardware.nix
+++ nixos/cfgs/lynx/hardware.nix
@@ -1,8 +1,19 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
-  boot.kernelParams = [
-    "amd_pstate=active" # Should stabilize clocks (was boosting to max 4.2GHz)
-  ];
+  boot = {
+    kernelParams = [
+      "amd_pstate=active" # Should stabilize clocks (was boosting to max 4.2GHz)
+    ];
+
+    extraModulePackages = [
+      (config.boot.kernelPackages.zenpower.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
+          pkgs.gcc
+        ];
+      }))
+    ];
+    kernelModules = [ "zenpower" ];
+  };
 
   hardware = {
     enableRedistributableFirmware = true;
