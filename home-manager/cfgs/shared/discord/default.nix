8e6802eee43770a653fdcacb94aed539d13b4127
Faupi <matej.sp583@gmail.com>
2026-06-16 01:03:17 +0200

    discord: switch to bleeding channel for fresh patches

--- home-manager/cfgs/shared/discord/default.nix
+++ home-manager/cfgs/shared/discord/default.nix
@@ -10,9 +10,10 @@ in
 
   config = mkMerge [
     (mkIf cfg.enable {
-      home.packages = with pkgs; [
-        discord
-      ];
+      programs.discord = {
+        enable = true;
+        package = pkgs.bleeding.discord; # 16/6/2026: Bleeding for latest patches like proper DMABUF support
+      };
 
       # NOTE: If using vesktop, change path to `vesktop/themes/midnight.theme.css`
       # xdg.configFile."Vencord/themes/midnight.theme.css".source = pkgs.vencord-midnight-theme;
