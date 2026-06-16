967002c3d7d35623867c154d908eaef7b23bd2dc
Faupi <matej.sp583@gmail.com>
2026-06-16 21:57:07 +0200

    Revert "lynx: switch to stock kernel for debugging suspend shutdowns"
    This reverts commit dd58336f381f1f444a7c76339317a7787f045bb4.

--- nixos/cfgs/lynx/boot.nix
+++ nixos/cfgs/lynx/boot.nix
@@ -1,23 +1,9 @@
 { pkgs, ... }: {
   boot = {
-    # REVIEW: Stock kernel and console suspend for suspend shutdown testing
-    # kernelPackages = pkgs.linuxKernel.packagesFor pkgs.cachyosKernels.linux-cachyos-latest-lto-x86_64-v3;
-    # Zenpower patch needed for cachyOS kernel
-    # extraModulePackages = [
-    #   (config.boot.kernelPackages.zenpower.overrideAttrs (old: {
-    #     nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
-    #       pkgs.llvmPackages.clang-unwrapped
-    #     ];
-
-    #     makeFlags = (old.makeFlags or [ ]) ++ [
-    #       "CC=${pkgs.llvmPackages.clang-unwrapped}/bin/clang"
-    #     ];
-    #   }))
-    # ];
+    kernelPackages = pkgs.linuxKernel.packagesFor pkgs.cachyosKernels.linux-cachyos-latest-lto-x86_64-v3;
     kernelParams = [
       "no_console_suspend"
     ];
-
     initrd.availableKernelModules = [
       "nvme"
       "xhci_pci"
