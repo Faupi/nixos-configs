bd7e6152770c0328ac0d1677a444378e1f3131d9
Faupi <matej.sp583@gmail.com>
2026-07-13 15:59:51 +0200

    discord: pitiful attempt at linking system vulkan drivers

--- home-manager/cfgs/shared/discord/default.nix
+++ home-manager/cfgs/shared/discord/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, fop-utils, ... }:
 with lib;
 let
   cfg = config.flake-configs.discord;
@@ -12,7 +12,20 @@ in
     (mkIf cfg.enable {
       programs.discord = {
         enable = true;
-        package = pkgs.bleeding.discord; # 16/6/2026: Bleeding for latest patches like proper DMABUF support
+        package = fop-utils.wrapPkgBinary {
+          inherit pkgs;
+          package = pkgs.bleeding.discord; # 16/6/2026: Bleeding for latest patches like proper DMABUF support
+
+          nameAffix = "hwaccel";
+
+          variables = {
+            # Target the system drivers if present (needed in case of system x home-manager mismatch)
+            VK_ADD_DRIVER_FILES = {
+              mode = "suffix";
+              value = "/run/opengl-driver/share/vulkan/icd.d";
+            };
+          };
+        };
       };
 
       # NOTE: If using vesktop, change path to `vesktop/themes/midnight.theme.css`
