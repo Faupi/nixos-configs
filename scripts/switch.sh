798898ee4aff10375314e853953dac4f2a7e47ac
Faupi <matej.sp583@gmail.com>
2026-07-22 12:35:23 +0200

    update switch script

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p bash jq
+#shellcheck shell=bash
 
-sudo nix flake update nixpkgs-bleeding
-sudo nixos-rebuild switch --flake "$(dirname "$0")" --refresh --show-trace --verbose "$@"
+flake=$(nix flake metadata --json . | jq -r .url)
+
+sudo true
+nix flake update nixpkgs-bleeding
+sudo nixos-rebuild switch --flake "$flake" --refresh --show-trace --verbose "$@" --log-format internal-json |& nom --json
