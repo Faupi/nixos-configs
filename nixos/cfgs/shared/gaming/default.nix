2008f95ffd05a2f66df97b889bbd33f21e4dabaf
Faupi <matej.sp583@gmail.com>
2026-07-27 17:22:08 +0200

    gaming: reenable libstrangle

--- nixos/cfgs/shared/gaming/default.nix
+++ nixos/cfgs/shared/gaming/default.nix
@@ -12,7 +12,7 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       mangohud
-      # libstrangle # Genuinely great at working around some games having broken VSync
+      libstrangle # Genuinely great at working around some games having broken VSync
 
       # IMPORTANT: as of 10/7/2026, enabling LSFG-VK can result in really weird game crashes (e.g. "CS rejected") and SUSPEND ISSUES (PC turning off while in suspend)
       # (pkgs.lsfg-vk_2.override { buildUI = true; })
