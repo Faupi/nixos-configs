3dc469090b2b2ea6553a9fe17da59b654a59a2eb
Faupi <matej.sp583@gmail.com>
2026-08-01 22:33:01 +0200

    gaming: improve gamemode notifs

--- nixos/cfgs/shared/gaming/default.nix
+++ nixos/cfgs/shared/gaming/default.nix
@@ -40,8 +40,8 @@ in
             notify = lib.getExe pkgs.libnotify;
           in
           {
-            start = "${notify} -t 3000 'GameMode started'";
-            end = "${notify} -t 3000 'GameMode ended'";
+            start = "${notify} --urgency=low --app-name=GameMode --expire-time=3000 'GameMode started'";
+            end = "${notify} --urgency=low --app-name=GameMode --expire-time=3000 'GameMode ended'";
           };
       };
     };
